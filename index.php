5228a58ee10d54356d67025b3c9d52e192f71df9
Tor Soya <torsoya@gmail.com>
2016-07-23 23:46:24 +0200

    index.php allowed

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Starter Template for Bootstrap 3.3.6</title>
+    <link rel="shortcut icon" href="">
+    <link rel="stylesheet" href="Style/bootstrap.min.css">
+    <link rel="stylesheet" href="Style/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="Style/hlpf_main.css">
+
+    <!--[if IE]>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <header>
+        <!-- Top start -->
+        <div class="container-fluid">
+            <div class="container">
+                <div class="row">
+                <div class="col-lg-4" id="hlpf_Logo"><img src="Images/logo.png"><br>Logo</div>
+                <div class="col-lg-4" id="hlpf_countdown"><img src="Images/logo.png"><br>Countdown</div>
+                <div class="col-lg-4" id="hlpf_Login"><img src="Images/logo.png"><br>Login</div>
+                </div>
+            </div>
+        </div>
+        <!-- Top end -->
+        <!-- Nav start -->
+        <nav class="navbar navbar-fixed" id="hlpf_menu" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse hlpf_white_text">
+                    <ul class="nav navbar-nav ">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div><!--.nav-collapse -->
+            </div>
+        </nav>
+        <!-- Nav end -->
+    </header>
+    <!-- Slider start -->
+    <div class="container-fluid">
+       <img src="Images/image-slider-5.jpg" width="100%" height="400px">
+    </div>
+    <!-- Slider end -->
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 hlpf_newsborder hlpf_min_large_height_news">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <p>
+                        Hvad er Lorem Ipsum?
+
+                        Lorem Ipsum er ganske enkelt fyldtekst fra print- og typografiindustrien. Lorem Ipsum har været standard fyldtekst siden 1500-tallet, hvor en ukendt trykker sammensatte en tilfældig spalte for at trykke en bog til sammenligning af forskellige skrifttyper. Lorem Ipsum har ikke alene overlevet fem århundreder, men har også vundet indpas i elektronisk typografi uden væsentlige ændringer. Sætningen blev gjordt kendt i 1960'erne med lanceringen af Letraset-ark, som indeholdt afsnit med Lorem Ipsum, og senere med layoutprogrammer som Aldus PageMaker, som også indeholdt en udgave af Lorem Ipsum.
+                            
+                        Lorem Ipsum er ganske enkelt fyldtekst fra print- og typografiindustrien. Lorem Ipsum har været standard fyldtekst siden 1500-tallet, hvor en ukendt trykker sammensatte en tilfældig spalte for at trykke en bog til sammenligning af forskellige skrifttyper. Lorem Ipsum har ikke alene overlevet fem århundreder, men har også vundet indpas i elektronisk typografi uden væsentlige ændringer. Sætningen blev gjordt kendt i 1960'erne med lanceringen af Letraset-ark, som indeholdt afsnit med Lorem Ipsum, og senere med layoutprogrammer som Aldus PageMaker, som også indeholdt en udgave af Lorem Ipsum.
+                        </p>
+                    </div>
+                    
+                </div>
+                <hr/>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <img src="Images/facebool_like.png">
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2"></div>
+            <div class="col-lg-5 hlpf_newsborder hlpf_min_large_height_news">eveevevrrbwrvsdv</div>
+        </div>
+    </div>
+
+    efef
+    wdw
+    
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</body>
+</html>
