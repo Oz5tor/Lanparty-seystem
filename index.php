4ed186865933c50c270211256f4e38e472ad1263
Tor Soya <torsoya@gmail.com>
2016-07-24 11:00:57 +0200

    added more stuff

--- index.php
+++ index.php
@@ -20,12 +20,12 @@
 <body>
     <header>
         <!-- Top start -->
-        <div class="container-fluid">
+        <div class="container">
             <div class="container">
                 <div class="row">
-                <div class="col-lg-4" id="hlpf_Logo"><img src="Images/logo.png"><br>Logo</div>
-                <div class="col-lg-4" id="hlpf_countdown"><img src="Images/logo.png"><br>Countdown</div>
-                <div class="col-lg-4" id="hlpf_Login"><img src="Images/logo.png"><br>Login</div>
+                <div class="col-lg-4 text-center" id="hlpf_Logo"><img src="Images/logo.png"><br>Logo</div>
+                <div class="col-lg-4 text-center" id="hlpf_countdown"><img src="Images/logo.png"><br>Countdown</div>
+                <div class="col-lg-4 text-center" id="hlpf_Login"><img src="Images/logo.png"><br>Login</div>
                 </div>
             </div>
         </div>
@@ -58,12 +58,15 @@
     </div>
     <!-- Slider end -->
     <br>
+    <!-- Headsponsor and Latest news start -->
     <div class="container">
         <div class="row">
-            <div class="col-lg-5 hlpf_newsborder hlpf_min_large_height_news">
+            <!-- Main sponsor post Start -->
+            <div class="col-lg-5 hlpf_newsborder">
                 <div class="row">
-                    <div class="col-lg-12">
+                    <div class="col-lg-12 hlpf_large_news_box">
                         <p>
+                        <center><img src="Images/logo.png"></center><hr/> 
                         Hvad er Lorem Ipsum?
 
                         Lorem Ipsum er ganske enkelt fyldtekst fra print- og typografiindustrien. Lorem Ipsum har været standard fyldtekst siden 1500-tallet, hvor en ukendt trykker sammensatte en tilfældig spalte for at trykke en bog til sammenligning af forskellige skrifttyper. Lorem Ipsum har ikke alene overlevet fem århundreder, men har også vundet indpas i elektronisk typografi uden væsentlige ændringer. Sætningen blev gjordt kendt i 1960'erne med lanceringen af Letraset-ark, som indeholdt afsnit med Lorem Ipsum, og senere med layoutprogrammer som Aldus PageMaker, som også indeholdt en udgave af Lorem Ipsum.
@@ -71,24 +74,143 @@
                         Lorem Ipsum er ganske enkelt fyldtekst fra print- og typografiindustrien. Lorem Ipsum har været standard fyldtekst siden 1500-tallet, hvor en ukendt trykker sammensatte en tilfældig spalte for at trykke en bog til sammenligning af forskellige skrifttyper. Lorem Ipsum har ikke alene overlevet fem århundreder, men har også vundet indpas i elektronisk typografi uden væsentlige ændringer. Sætningen blev gjordt kendt i 1960'erne med lanceringen af Letraset-ark, som indeholdt afsnit med Lorem Ipsum, og senere med layoutprogrammer som Aldus PageMaker, som også indeholdt en udgave af Lorem Ipsum.
                         </p>
                     </div>
-                    
                 </div>
                 <hr/>
+            <!-- Facebook Social like thingy start -->
                 <div class="row">
                     <div class="col-lg-12">
-                        <img src="Images/facebool_like.png">
+                        <img src="Images/facebool_like.png" height="46">
                     </div>
                 </div>
+                <br>
+            <!-- Facebook Social like thingy end -->
             </div>
+            <!-- Main sponsor post End -->
             <div class="col-lg-2"></div>
-            <div class="col-lg-5 hlpf_newsborder hlpf_min_large_height_news">eveevevrrbwrvsdv</div>
+            <!-- Lastest News start -->
+            <div class="col-lg-5 hlpf_newsborder">
+                <div class="row">
+                    <div class="col-lg-12 hlpf_large_news_box">
+                        <p>
+                            <center><img src="Images/logo.png"></center><hr/>
+                            Hvad er Lorem Ipsum?
+
+                            Lorem Ipsum er ganske enkelt fyldtekst fra print- og typografiindustrien. Lorem Ipsum har været standard fyldtekst siden 1500-tallet, hvor en ukendt trykker sammensatte en tilfældig spalte for at trykke en bog til sammenligning af forskellige skrifttyper. Lorem Ipsum har ikke alene overlevet fem århundreder, men har også vundet indpas i elektronisk typografi uden væsentlige ændringer. Sætningen blev gjordt kendt i 1960'erne med lanceringen af Letraset-ark, som indeholdt afsnit med Lorem Ipsum, og senere med layoutprogrammer som Aldus PageMaker, som også indeholdt en udgave af Lorem Ipsum.
+
+                            Lorem Ipsum er ganske enkelt fyldtekst fra print- og typografiindustrien. Lorem Ipsum har været standard fyldtekst siden 1500-tallet, hvor en ukendt trykker sammensatte en tilfældig spalte for at trykke en bog til sammenligning af forskellige skrifttyper. Lorem Ipsum har ikke alene overlevet fem århundreder, men har også vundet indpas i elektronisk typografi uden væsentlige ændringer. Sætningen blev gjordt kendt i 1960'erne med lanceringen af Letraset-ark, som indeholdt afsnit med Lorem Ipsum, og senere med layoutprogrammer som Aldus PageMaker, som også indeholdt en udgave af Lorem Ipsum.
+                        </p>
+                    </div>
+                </div>
+                <hr>
+                <!-- News archive button start -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <button type="button" class="btn btn-primary btn-lg btn-block">
+                            Nyhedsarkiv
+                        </button>
+                    </div>
+                </div>
+                <br>
+                <!-- News archive button end -->  
+            </div>
+            <!-- Lastest News End -->
         </div>
     </div>
-
-    efef
-    wdw
+    <!-- Headsponsor and Latest news end -->  
+    <!-- Tournaments start -->
+    <hr>
+    <div class="container">
+        
+        <div class="row hlpf_tournament_block">
+            <div class="col-lg-2 col-xs-6 col-md-2">
+                <a href="#"><img src="Images/games/counter_strike__global_offensive_by_blitzk93-d6za56v.png"></a>
+            </div>
+            <div class="col-lg-2 col-xs-6 col-md-2">
+                <a href="#"><img src="Images/games/hearthstone__heroes_of_warcraft___icon_by_blagoicons-d6tqo08.png"></a>
+            </div>
+            <div class="col-lg-2 col-xs-6 col-md-2">
+                <a href="#"><img src="Images/games/league_of_legends___icon_by_darhymes-d4th6kj.png"></a>
+            </div>
+            <div class="col-lg-2 col-xs-6 col-md-2">
+                <a href="#"><img src="Images/games/ow.png"></a>
+            </div>
+            <div class="col-lg-2 col-xs-6 col-md-2">
+                <a href="#"><img src="Images/games/Retro-Block-Question-2-icon.png"></a>
+            </div>
+            <div class="col-lg-2 col-xs-6 col-md-2">
+                <a href="#"><img src="Images/games/rocket_league_by_pooterman-d90cnml.png"></a>
+            </div>
+        </div>
+        
+    </div>
+    <!-- Tournaments end -->
+    <hr>
+    <br>
+    <!-- Menu tiles start -->
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6 col-md-3">
+                <a href="#" class="thumbnail hlpf_no_round_border">
+                  <img src="Images/logo.png">
+                </a>    
+            </div>
+            <div class="col-xs-6 col-md-3">
+                <a href="#" class="thumbnail hlpf_no_round_border">
+                  <img src="Images/logo.png">
+                </a>    
+            </div>
+            <div class="col-xs-6 col-md-3">
+                <a href="#" class="thumbnail hlpf_no_round_border">
+                  <img src="Images/logo.png">
+                </a>    
+            </div>
+            <div class="col-xs-6 col-md-3">
+                <a href="#" class="thumbnail hlpf_no_round_border">
+                  <img src="Images/logo.png">
+                </a>    
+            </div>
+        </div>
+    </div>
+    <!-- Menu tiles end -->
     
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center"><h2>Hoved Sponsor</h2></div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <img src="Images/Sponsore/bequiet.gif">
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center"><h3>Sponsore</h3></div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <a href="#" class="thumbnail hlpf_no_round_border">
+                  <img src="Images/Sponsore/bequiet.gif">
+                </a>    
+            </div>
+            <div class="col-lg-4">
+                <a href="#" class="thumbnail hlpf_no_round_border">
+                  <img src="Images/Sponsore/bequiet.gif">
+                </a>    
+            </div>
+            <div class="col-lg-4">
+                <a href="#" class="thumbnail hlpf_no_round_border">
+                  <img src="Images/Sponsore/bequiet.gif">
+                </a>    
+            </div>
+        </div>
+    </div>
+    <nav class="navbar navbar-bottom" id="hlpf_menu">
+  <div class="container">
     
+  </div>
+</nav>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </body>
