c1dff6b1e910145da5bcc4ae7ec3db41026c4a9d
Tor Soya <torsoya@gmail.com>
2016-08-03 09:45:29 +0200

    menu tiles vs game icon ratio
    improved the menu tiles vs game icon ration  due to max-width 100% from
    .img-responsive is dam too large.

--- index.php
+++ index.php
@@ -139,24 +139,24 @@
     <div class="container">        
         <div class="row">
             <div class="col-lg-2 col-xs-4 col-md-2 col-sm-2 ">
-                <a href="#"><img class="img-responsive center-block" src="Images/games/counter_strike__global_offensive_by_blitzk93-d6za56v.jpg"></a>
+                <a href="#"><img class="img-responsive center-block" src="Images/games/counter_strike__global_offensive_by_blitzk93-d6za56v.jpg" width="100"></a>
             </div>
             <div class="col-lg-2 col-xs-4 col-md-2 col-sm-2 ">
-                <a href="#"><img class="img-responsive center-block" src="Images/games/hearthstone__heroes_of_warcraft___icon_by_blagoicons-d6tqo08.jpg"></a>
+                <a href="#"><img class="img-responsive center-block" src="Images/games/hearthstone__heroes_of_warcraft___icon_by_blagoicons-d6tqo08.jpg" width="100"></a>
             </div>
             <div class="col-lg-2 col-xs-4 col-md-2 col-sm-2 ">
-                <a href="#"><img class="img-responsive center-block" src="Images/games/league_of_legends___icon_by_darhymes-d4th6kj.jpg"></a>
+                <a href="#"><img class="img-responsive center-block" src="Images/games/league_of_legends___icon_by_darhymes-d4th6kj.jpg" width="100"></a>
             </div>
             <div class="col-lg-2 col-xs-4 col-md-2 col-sm-2 ">
-                <a href="#"><img class="img-responsive center-block" src="Images/games/ow.jpg"></a>
+                <a href="#"><img class="img-responsive center-block" src="Images/games/ow.jpg" width="100"></a>
             </div>
             <div class="col-lg-2 col-xs-4 col-md-2 col-sm-2 ">
-                <a href="#"><img class="img-responsive center-block" src="Images/games/Retro-Block-Question-2-icon.jpg"></a>
+                <a href="#"><img class="img-responsive center-block" src="Images/games/Retro-Block-Question-2-icon.jpg" width="100"></a>
             </div>
             <div class="col-lg-2 col-xs-4 col-md-2 col-sm-2  ">
-                <a href="#"><img class="img-responsive center-block" src="Images/games/rocket_league_by_pooterman-d90cnml.jpg"></a>
+                <a href="#"><img class="img-responsive center-block" src="Images/games/rocket_league_by_pooterman-d90cnml.jpg" width="100"></a>
             </div>
-        </div>  
+        </div>
     </div>
     <!-- Tournaments end -->
     <br>
