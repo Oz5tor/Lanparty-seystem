e49f9ac7b1b65e05ae2981b502d0f4d998f65700
Tor Soya <torsoya@gmail.com>
2016-12-01 09:31:22 +0100

    Seperation of code #forgotIndexCommit

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
-require_once("/include/DBconn.php");
+ob_start();
+session_start();
+require_once("/Include/DBconn.php");
 require_once("/oneall_sdk/config.php");
-require_once("/include/oneall_hlpf/oneall_calls.php");
+require_once("/Include/oneall_hlpf/oneall_calls.php");
+
+echo '<pre>';
+echo print_r($_SESSION);
+echo '</pre>';
 
 ?>
 <!DOCTYPE html>
@@ -33,22 +39,6 @@ require_once("/include/oneall_hlpf/oneall_calls.php");
     </script>
 </head>
 <body>
-    
-    <!-- ===================================== -->
-    <div id="oa_social_login_container"></div>
-      <script type="text/javascript"> 
-        /* Replace #your_callback_uri# with the url to your own callback script */
-        var your_callback_script = 'https://localhost/Website-2017/Include/oneall_hlpf/oneall_callback_handler.php';
-        /* Embeds the buttons into the container oa_social_login_container */
-        var _oneall = _oneall || [];
-        _oneall.push(['social_login', 'set_providers', ['facebook', 'google']]);
-        _oneall.push(['social_login', 'set_callback_uri', your_callback_script]);
-        _oneall.push(['social_login', 'do_render_ui', 'oa_social_login_container']);
-      </script>
-    
-    <!-- ===================================== -->
-    
-    
     <!-- Facebook scocial like code prep start -->
     <div id="fb-root"></div>
     <script>(function(d, s, id) {
@@ -67,254 +57,25 @@ require_once("/include/oneall_hlpf/oneall_calls.php");
     <br>
     <header>
         <!-- Top start -->
-        <div class="container">
-            <div class="container">
-                <div class="row">
-                <div class="hidden-xs col-lg-4 col-md-4 col-sm-4 col-xs-12 text-center" id="hlpf_Logo"><img class="img-responsive" src="http://placehold.it/390x80"></div>
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 text-center" id="hlpf_countdown">
-                    <div class="tc_div_75237" style="width:100%;height:auto;border:1px solid #C0C0C0"><a title="HLParty #25" href="http://www.tickcounter.com/widget/countdown/1476867600000/europe-berlin/dhms/2060C0FFFFFF000000FF0000/125/C0C0C01/HLParty_%2325">Dummy count down</a><a title="Countdown" href="http://www.tickcounter.com/">Countdown</a></div><script type="text/javascript">(function(){ var s=document.createElement('script');s.src="http://www.tickcounter.com/loader.js";s.async='async';s.onload=function() { tc_widget_loader('tc_div_75237', 'Countdown', 650, ["1476867600000","europe-berlin","dhms","2060C0FFFFFF000000FF0000","0","C0C0C01","HLParty #25"]);};s.onreadystatechange=s.onload;var head=document.getElementsByTagName('head')[0];head.appendChild(s);}());</script>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 text-center hidden-xs" id="hlpf_Login"><img  class="img-responsive" src="http://placehold.it/390x80"></div>
-                </div>
-            </div>
-        </div>
+        <?php require_once("/Include/TopHeader.php"); ?>
         <!-- Top end -->
+        <br>
         <!-- Nav start -->
-        <nav class="navbar navbar-fixed" id="hlpf_menu" role="navigation">
-            <div class="container">
-                <div class="navbar-header">
-                    <a class="visible-xs navbar-brand" href="#">
-                        <img alt="HLParty" class="img-responsive" src="Images/logo-brand.png">
-                    </a>
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <i class="fa fa-bars fa-2x" aria-hidden="true"></i>
-                    </button>
-                </div>
-                <div class="collapse navbar-collapse hlpf_white_text">
-                    <ul class="nav navbar-nav ">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li class="dropdown" id="hlpf_menu">
-                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">HLParty <span class="caret"></span></a>
-                          <ul class="dropdown-menu">
-                            <li><a href="#">Information</a></li>
-                            <li><a href="#">Regler</a></li>
-                            <li><a href="#">Crew</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="#">Kantinen tilbyder</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="#">Yderliger information</a></li>
-                            <li><a href="#">Find Os</a></li>
-                            <li><a href="#">Huskeliste</a></li>
-                          </ul>
-                        </li>
-                        <li><a href="#contact">Contact</a></li>
-                    </ul>
-                </div><!--.nav-collapse -->
-            </div>
-        </nav>
+        <?php require_once("/Include/NavigationBar.php"); ?>
         <!-- Nav end -->
     </header>
     <br>
-    <!-- Headsponsor and Latest news start -->
     <div class="container">
-        <div class="row">
-            <!-- Main sponsor post Start -->
-            <div class="col-lg-5 hlpf_newsborder">
-                <div class="row">
-                    <div class="col-lg-12 hlpf_large_news_box">
-                        <img class="img-responsive" src="Images/image-slider-5.jpg">
-                        <hr/> 
-                        <div class="hlpf_flex">
-                            <div class="hlpf_news">
-                                POWER er Danmarks nye elektronikkæde, og vi kommer over de næste par år til at tage Danmark med storm. Alene i 2015 åbnede vi fire spritnye, store POWER houses i hovedstadsområdet samt et i Odense. Men vi stopper ikke her. I løbet af de næste par år vil en lang række nye POWER houses skyde op over alt i landet til glæde for pris- og kvalitetsbevidste danskere.
-                                <br />
-                                Vores mission er at gøre det til en oplevelse at købe elektronik. Derfor er POWER et sted fyldt med energi, begejstring og vigtigst af alt god service. Vi har nemlig de mest engagerede sælgere, de dygtigste ledere og de mest smilende kassemedarbejdere. Vi kan nemlig lide det, vi laver.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <hr/>
-            <!-- Facebook Social like thingy start -->
-                <div class="row">
-                    <div class="hlpf_facebok_like col-lg-12 col-sm-12 col-md-12 col-xs-12 text-center">
-                        <div class="fb-like" data-href="https://www.facebook.com/HLParty" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="true"></div>
-                    </div>
-                    
-                </div>
-                <br>
-            <!-- Facebook Social like thingy end -->
-            </div>
-            <!-- Main sponsor post End -->
-            <div class="col-lg-2"></div>
-            <!-- Lastest News start -->
-            <div class="col-lg-5 hlpf_newsborder">
-                <div class="row">
-                    <div class="col-lg-12 hlpf_large_news_box">
-                        <img class="img-responsive"src="Images/image-slider-5.jpg">
-                        <hr/>
-                        <div class="hlpf_flex">
-                            <div class="hlpf_news">
-                                <p>
-                                <b>HLParty takker for et vellykket event.</b>
-                                <br>
-                                    <br>
-                                Tak til alle sponsorer, samarbejdspartnere og deltagere, som var med til at gøre HLParty #25 - POWER.DK til et fedt lan!
-                                <br>
-                                    <br>
-                                Vi modtager meget gerne ris og ros fra alle vores deltagere, så brug 5 minutter på at udfylde dette spørgeskema, så udtrækker vi blandt alle besvarelser en fribillet til næste HLParty (husk at opgive dit brugernavn, hvis du ønsker at vinde).
-                                    
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <hr>
-                <!-- News archive button start -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <button type="button" class="btn btn-primary btn-xs btn-block">
-                            Nyhedsarkiv
-                        </button>
-                    </div>
-                </div>
-                <br>
-                <!-- News archive button end -->  
-            </div>
-            <!-- Lastest News End -->
-        </div>
+    <?php require_once("/Include/PageCaller.php"); ?>
     </div>
-    <!-- Headsponsor and Latest news end -->  
-    <!-- Tournaments start -->
-    <hr>
-    <div class="container">        
-        <div class="row">
-            <div class="col-lg-2 col-xs-4 col-md-2 col-sm-2 hlpf_margin_botom ">
-                <a href="#"><img class="img-responsive center-block" src="Images/games/counter_strike__global_offensive_by_blitzk93-d6za56v.jpg" width="100"></a>
-            </div>
-            <div class="col-lg-2 col-xs-4 col-md-2 col-sm-2 hlpf_margin_botom ">
-                <a href="#"><img class="img-responsive center-block" src="Images/games/hearthstone__heroes_of_warcraft___icon_by_blagoicons-d6tqo08.jpg" width="100"></a>
-            </div>
-            <div class="col-lg-2 col-xs-4 col-md-2 col-sm-2 hlpf_margin_botom ">
-                <a href="#"><img class="img-responsive center-block" src="Images/games/league_of_legends___icon_by_darhymes-d4th6kj.jpg" width="100"></a>
-            </div>
-            <div class="col-lg-2 col-xs-4 col-md-2 col-sm-2 hlpf_margin_botom ">
-                <a href="#"><img class="img-responsive center-block" src="Images/games/ow.jpg" width="100"></a>
-            </div>
-            <div class="col-lg-2 col-xs-4 col-md-2 col-sm-2 hlpf_margin_botom ">
-                <a href="#"><img class="img-responsive center-block" src="Images/games/Retro-Block-Question-2-icon.jpg" width="100"></a>
-            </div>
-            <div class="col-lg-2 col-xs-4 col-md-2 col-sm-2 hlpf_margin_botom  ">
-                <a href="#"><img class="img-responsive center-block" src="Images/games/rocket_league_by_pooterman-d90cnml.jpg" width="100"></a>
-            </div>
-        </div>
-    </div>
-    <!-- Tournaments end -->
-    <br>
-    <!-- Menu tiles start -->
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-6 col-md-3 col-sm-3 hlpf_margin_botom">
-                <a href="#" class="">
-                  <img class="img-responsive img-thumbnail center-block" src="Images/games/turneringer.jpg">
-                </a>    
-            </div>
-            <div class="col-xs-6 col-md-3 col-sm-3 hlpf_margin_botom">
-                <a href="#" class="">
-                  <img class="img-responsive img-thumbnail center-block" src="Images/games/turneringer.jpg">
-                </a>    
-            </div>
-            <div class="col-xs-6 col-md-3 col-sm-3 hlpf_margin_botom">
-                <a href="#" class="">
-                  <img class="img-responsive img-thumbnail center-block" src="Images/games/turneringer.jpg">
-                </a>    
-            </div>
-            <div class="col-xs-6 col-md-3 col-sm-3 hlpf_margin_botom">
-                <a href="#" class="">
-                  <img class="img-responsive img-thumbnail center-block" src="Images/games/turneringer.jpg">
-                </a>    
-            </div>
-        </div>
-    </div>
-    <!-- Menu tiles end -->
+    <?php require_once("/Include/TilesAndTournament.php"); ?>
     <!-- Sponsors start -->
     <hr>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 col-xs-12 text-center"><h2>Hoved Sponsor</h2></div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12 col-xs-12 text-center">
-                <a href="#" class="thumbnail hlpf_no_round_border">
-                  <img src="Images/Sponsore/bequiet.gif">
-                </a>
-            </div>
-        </div>
-    </div>
-    
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 text-center"><h3>Sponsore</h3></div>
-        </div>
-        <div class="row">
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                <a href="#" class="thumbnail">
-                  <img class="img-responsive" src="Images/Sponsore/bequiet.gif">
-                </a>    
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                <a href="#" class="thumbnail">
-                  <img class="img-responsive" src="Images/Sponsore/geekunit.png">
-                </a>    
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                <a href="#" class="thumbnail">
-                  <img class="img-responsive" src="Images/Sponsore/SSNetwork.png">
-                </a>    
-            </div>
-        </div>
-    </div>
+    <?php require_once("/Include/Sponsors.php"); ?>
     <!-- Sponsors end -->
     <!-- Footer start -->
-    <footer class="container-fluid" id="hlpf_footer">
-        <div class="container">
-            <div class="row">
-            <!-- Google Maps -->
-            <div class="hidden-xs col-sm-12 col-md-6">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2234.58538684431!2d12.287926209023182!3d55.9392252493907!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465240852b497295%3A0x2ae4a89f1e73f438!2sHiller%C3%B8dsholmskolen!5e0!3m2!1sda!2sdk!4v1469351469692" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
-            </div>
-            <!-- Generel information, kontaktinformation og sociale medier -->
-            <div class="col-sm-12 col-md-6">
-                <!-- Adresse og kontakt -->
-                <div class="col-lg-6 hidden-xs hidden-sm hidden-md" id="hlpf_footer_contact">
-                    <h2>Adresse</h2>
-                    <address>
-                        Hillerødsholmskolen <br>
-                        Hillerødsholmsallé 2 <br>
-                        3400 Hillerød
-                    </address>
-                    <h2>Kontakt</h2>
-                    <p>Bestyrelsen kan kontaktes på <a href="mailto:best@hlpf.dk">best@hlpf.dk</a>.<br> Ellers henviser vi til vores kontaktside.</p>
-                </div>
-                <!-- Generelle links -->
-                <div class="col-lg-6">
-                    <h2>Nyttige links</h2>
-                    <ul id="hlpf_footer_links">
-                        <li><a href="#">Regler</a></li>
-                        <li><a href="#">Konkurrencer</a></li>
-                        <li><a href="#">Kantinen</a></li>
-                    </ul>
-                    <!-- Sociale links -->
-                    <h2>Social</h2>
-                    <a href="#"><span class="fa fa-facebook fa-3x"></span></a>&nbsp;
-                    <a href="#"><span class="fa fa-twitter fa-3x"></span></a>&nbsp;
-                    <a href="#"><span class="fa fa-youtube fa-3x"></span></a>
-                </div>
-            </div>        
-        </div>
-    </footer>
+    <?php require_once("/Include/Footer.php"); ?>
+    <!-- Footer end -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </body>
