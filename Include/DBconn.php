7be39839bf1308c7294e6c554df5e40f41f43235
Tor Soya <torsoya@gmail.com>
2016-12-04 21:55:47 +0100

    forgot to move over to prodution db

--- Include/DBconn.php
+++ Include/DBconn.php
@@ -2,7 +2,7 @@
     $db_host="localhost"; // Host name
     $db_username="root"; // Mysql username
     $db_password=""; // Mysql password
-    $db_name="oneall_testing"; // Database name
+    $db_name="hlparty2018"; // Database name
     $db_conn = new mysqli("$db_host","$db_username","$db_password","$db_name");
     $db_conn -> set_charset("utf8");
 ?> 
