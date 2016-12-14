2be5d9c582ab9607f181239c628cae3094bda74f
Rosenheim <patrickrosenheim@gmail.com>
2016-12-14 10:58:42 +0100

    DB connect

--- Include/DBconn.php
+++ Include/DBconn.php
@@ -1,8 +1,8 @@
 <?php
-    $db_host="localhost"; // Host name
-    $db_username="root"; // Mysql username
-    $db_password=""; // Mysql password
-    $db_name="hlparty2018"; // Database name
+    $db_host="82.147.224.26"; // Host name
+    $db_username="hlparty"; // Mysql username
+    $db_password="hlparty"; // Mysql password
+    $db_name="hlparty"; // Database name
     $db_conn = new mysqli("$db_host","$db_username","$db_password","$db_name");
     $db_conn -> set_charset("utf8");
 ?> 
