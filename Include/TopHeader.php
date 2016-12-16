a810bc3a73db6e3a432265a714a0faf269982453
Rosenheim <patrickrosenheim@gmail.com>
2016-12-16 09:55:54 +0100

    DBconn

--- Include/TopHeader.php
+++ Include/TopHeader.php
@@ -3,7 +3,7 @@
         <div class="row">
         <div class="hidden-xs col-lg-4 col-md-4 col-sm-4 col-xs-12 text-center" id="hlpf_Logo"><img class="img-responsive" src="http://placehold.it/390x80"></div>
         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 text-center" id="hlpf_countdown">
-            <div class="tc_div_9805" style="height:80px;"><a title="HLParty #25" href="//www.tickcounter.com/widget/countdown/1476867600000/europe-berlin/dhms/2060C0FFFFFF000000FF0000/125/C0C0C01/HLParty_%2325">HLParty #25</a><a title="Countdown" href="https://www.tickcounter.com/">Countdown</a></div><script type="text/javascript">(function(){ var s=document.createElement('script');s.src="//www.tickcounter.com/loader.js";s.async='async';s.onload=function() { tc_widget_loader('tc_div_9805', 'Countdown', 125, ["1476860400000","europe-berlin","dhms","2060C0FFFFFF000000FF0000","0","C0C0C01","HLParty #25"]);};s.onreadystatechange=s.onload;var head=document.getElementsByTagName('head')[0];head.appendChild(s);}());</script>
+            <h3>Middle tekst</h3>
         </div>
         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 text-center hidden-xs" id="hlpf_Login">
             <!-- ===================================== -->
