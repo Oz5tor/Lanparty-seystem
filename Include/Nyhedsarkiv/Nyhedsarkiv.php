683c5ea1db8287c85aa5c119023824606f5e31f9
MarkLenti <vikingemig@hotmail.com>
2017-01-03 12:17:48 +0100

    Added basic pagination
    Not dynamic yet

--- Include/Nyhedsarkiv/Nyhedsarkiv.php
+++ Include/Nyhedsarkiv/Nyhedsarkiv.php
@@ -5,9 +5,16 @@
 		</p>
 	</div>
 	<div class="row">
-		<p>
-			Sider her
-		</p>
+		<ul class="pagination pagination-sm">
+			<li><a href="#">&laquo;</a></li>
+			<li><a href="#">1</a></li>
+			<li><a href="#">2</a></li>
+			<li><a href="#">3</a></li>
+			<li><a href="#">4</a></li>
+			<li><a href="#">5</a></li>
+			<li><a href="#">6</a></li>
+			<li><a href="#">&raquo;</a></li>
+		</ul>
 	</div>
 	<div class="row">
 		<p>
