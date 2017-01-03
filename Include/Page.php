da0e218c29afb9dbe3ccec16b3a2a5e4899a5a29
Rosenheim <patrickrosenheim@gmail.com>
2017-01-03 09:36:15 +0100

    More fix

--- Include/Page.php
+++ Include/Page.php
@@ -1,29 +1,46 @@
 <div class="row">
     <div class="col-lg-12 hlpf_newsborder">
         <?php
-            if( $result = $db_conn->query( "
-                SELECT
-                  P.Content,
-                  P.PageTitle,
-                  U1.Username AS Creator,
-                  U2.Username AS Editor,
-                  P.CreatedDate,
-                  P.LastEditedDate
-                FROM
-                  Pages P INNER JOIN
-                  Users U1
-                    ON P.AuthorID = U1.UserID INNER JOIN
-                  Users U2
-                    ON P.LastEditedID = U2.UserID
-                WHERE
-                  P.PageID = $page
-            " ) ){
-                if( $result -> num_rows ){
-                    $row = $result->fetch_assoc();
-                } else {
-                    // GOTO 404 PAGE
-                }
-            }
+          if( ctype_digit( strval( $page ) ) ) {
+            $result = $db_conn->query( "
+              SELECT
+                P.Content,
+                P.PageTitle,
+                U1.Username AS Creator,
+                U2.Username AS Editor,
+                P.CreatedDate,
+                P.LastEditedDate
+              FROM Pages P
+                INNER JOIN Users U1
+                  ON P.AuthorID = U1.UserID
+                INNER JOIN Users U2
+                  ON P.LastEditedID = U2.UserID
+                WHERE P.PageID = $page
+              " ); // End query.
+          } elseif (! ctype_digit( strval( $page ) )) {
+            $result = $db_conn->query( "
+              SELECT
+                P.Content,
+                P.PageTitle,
+                U1.Username AS Creator,
+                U2.Username AS Editor,
+                P.CreatedDate,
+                P.LastEditedDate
+              FROM Pages P
+                INNER JOIN Users U1
+                  ON P.AuthorID = U1.UserID
+                INNER JOIN Users U2
+                  ON P.LastEditedID = U2.UserID
+                WHERE P.PageTitle = '$page'
+            " ); // End query.
+          }
+
+          if( $result -> num_rows ){
+            $row = $result->fetch_assoc();
+          } else {
+            // TO THE FRONT-PAGE!
+            header("Location: /Website-2017/index.php");
+          }
         ?>
         <div class="row">
             <div class="col-lg-12 hlpf_large_news_box">
@@ -41,5 +58,6 @@
         </div>
         <hr/>
         <?php $result -> close(); ?>
+        
     </div>
 </div>
