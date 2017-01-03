401222a80c0d78996ed6270f2a20cd732e4574f7
Tor Soya <torsoya@gmail.com>
2017-01-03 14:49:07 +0100

    Script Paths changed

--- index.php
+++ index.php
@@ -92,7 +92,7 @@ include_once("Include/DEBUGGIN.php");
     <!-- Footer start -->
     <?php require_once("Include/Footer.php"); ?>
     <!-- Footer end -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="JS/Jquery/jquery.min.js"></script>
+    <script src="JS/Bootstrap/bootstrap.min.js"></script>
 </body>
 </html>
