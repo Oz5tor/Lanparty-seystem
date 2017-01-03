b9f08ec873b36fbc8fcd14cf06ee50a451cb0e08
MarkLenti <vikingemig@hotmail.com>
2017-01-03 14:50:20 +0100

    Adding news database query
    With minimal styling

--- Include/Nyhedsarkiv/Nyhedsarkiv.php
+++ Include/Nyhedsarkiv/Nyhedsarkiv.php
@@ -1,10 +1,10 @@
 <div class="col-lg-12 hlpf_newsborder">
-	<div class="row">
+	<div class="row col-lg-12">
 		<p>
 			Nyhedsarkiv
 		</p>
 	</div>
-	<div class="row">
+	<div class="row col-lg-12">
 		<ul class="pagination pagination-sm">
 			<li><a href="#">&laquo;</a></li>
 			<li><a href="#">1</a></li>
@@ -18,7 +18,24 @@
 	</div>
 	<div class="row">
 		<p>
-			Nyheder her
+			<?php
+			$result = $db_conn->query( "
+			    SELECT
+			      News.Title, News.Content
+			    FROM News ORDER BY CreatedDate DESC
+			");
+			?>
+			<?php while ($row = $result->fetch_assoc()) { ?>
+				<div class="col-lg-12">
+					<div class="col-lg-12">
+						<h2><?php echo $row['Title']?></h2>
+					</div>
+					<div class="col-lg-12">
+						<?php echo $row['Content']?>
+					</div>
+					<hr>
+				</div>
+			<?php } ?>
 		</p>
 	</div>
 </div>
