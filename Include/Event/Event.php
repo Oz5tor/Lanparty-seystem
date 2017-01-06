ecba3817191efac031140e57c7cd17209bf3efba
MarkLenti <vikingemig@hotmail.com>
2017-01-06 08:02:13 +0100

    Not done commit of event
    Committing some design changes to events

--- Include/Event/Event.php
+++ Include/Event/Event.php
@@ -8,28 +8,48 @@
 ?>
 
 <div class="col-lg-12 hlpf_newsborder"> <!-- Ret class til-->
-	<div class="row col-lg-12">
-		<h2><?php echo $row['Title'] ?></h2>
+	<div class="col-lg-6 row">
+		<div class="row col-lg-12">
+			<p><h2>Information:</h2></p>
+		</div>
+		<div class="row col-lg-12">
+			<p><h4>Tid og sted:</h4></p>
+		</div>
+		<div class="row col-lg-4">
+			<p>Navn:</p>
+		</div>
+		<div class="row col-lg-8">
+			<p><?php echo $row['Title'] ?></p>
+		</div>
+		<div class="row col-lg-12">
+			<p>Dato: <?php echo date("d", $row['StartDate']) ?> - <?php echo date("d M Y", $row['EndDate']) ?></p>
+		</div>
+		<div class="row col-lg-12">
+			<p>Start: <?php echo date("d M Y - H:i:s", $row['StartDate']) ?></p>
+		</div>
+		<div class="row col-lg-12">
+			<p>Slut: <?php echo date("d M Y - H:i:s", $row['EndDate']) ?></p>
+		</div>
+		<div class="row col-lg-12">
+			<p>Adresse: <?php echo $row['Location'] ?></p>
+		</div>
+		<div class="row col-lg-12">
+			<p>Tilmelding begynder den <?php echo date("d M Y - H:i:s", $row['SeatsOpen']) ?>.</p>
+		</div>
+		<div class="row col-lg-12">
+			<p>Arrangementet finder sted på addressen <?php echo $row['Location'] ?> og vil vare fra den 
+			<?php echo date("d M Y - H:i:s", $row['StartDate']) ?> til den <?php echo date("d M Y - H:i:s", $row['EndDate']) ?>.</p>
+		</div>
+		<div class="row col-lg-12">
+			<button class="button">Tilmelding</button>
+		</div>
 	</div>
-	<div class="row col-lg-12">
-		Event brødtekst.
-		<br>
-		<br>
-	</div>
-	<div class="row col-lg-4">
-		Tilmelding begynder den <?php echo date("d M Y - H:i:s", $row['SeatsOpen']) ?>.
-		<br>
-		<br>
-	</div>
-	<div class="row col-lg-8">
-		<?php echo $row['Poster'] ?> <!-- Der skal måske lige noget til for faktisk at vise et billede her -->
-	</div>
-	<div class="row col-lg-11">
-		Arrangementet finder sted på addressen <?php echo $row['Location'] ?> og vil vare fra den 
-		<?php echo date("d M Y - H:i:s", $row['StartDate']) ?> til den <?php echo date("d M Y - H:i:s", $row['EndDate']) ?>.
-	</div>
-	<div class="row col-lg-1">
-		<button class="button">Tilmelding</button>
+	<div class="col-lg-6 row">
+		<div class="row col-lg-12">
+			<?php echo '<img class="img-responsive" src="Images/Sponsore/'.$row['Poster'] . '">' ?>
+			<br>
+			<br>
+		</div>
 	</div>
 </div>
 
