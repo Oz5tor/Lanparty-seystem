0fcf628d020ebb4aa237be7c8c31e2476bbdf544
MarkLenti <vikingemig@hotmail.com>
2017-01-06 08:28:52 +0100

    More design for event
    Committing changes, so that I can update from master

--- Include/Event/Event.php
+++ Include/Event/Event.php
@@ -12,6 +12,8 @@
 		<div class="row col-lg-12">
 			<p><h2>Information:</h2></p>
 		</div>
+
+		<!-- Tid og sted -->
 		<div class="row col-lg-12">
 			<p><h4>Tid og sted:</h4></p>
 		</div>
@@ -21,34 +23,56 @@
 		<div class="row col-lg-8">
 			<p><?php echo $row['Title'] ?></p>
 		</div>
-		<div class="row col-lg-12">
-			<p>Dato: <?php echo date("d", $row['StartDate']) ?> - <?php echo date("d M Y", $row['EndDate']) ?></p>
+		<div class="row col-lg-4">
+			<p>Dato:</p>
 		</div>
-		<div class="row col-lg-12">
-			<p>Start: <?php echo date("d M Y - H:i:s", $row['StartDate']) ?></p>
+		<div class="row col-lg-8">
+			<p><?php echo date("d", $row['StartDate']) ?> - <?php echo date("d M Y", $row['EndDate']) ?></p>
 		</div>
-		<div class="row col-lg-12">
-			<p>Slut: <?php echo date("d M Y - H:i:s", $row['EndDate']) ?></p>
+		<div class="row col-lg-4">
+			<p>Start:</p>
 		</div>
-		<div class="row col-lg-12">
-			<p>Adresse: <?php echo $row['Location'] ?></p>
+		<div class="row col-lg-8">
+			<p><?php echo date("d M Y - H:i:s", $row['StartDate']) ?></p>
 		</div>
-		<div class="row col-lg-12">
-			<p>Tilmelding begynder den <?php echo date("d M Y - H:i:s", $row['SeatsOpen']) ?>.</p>
+		<div class="row col-lg-4">
+			<p>Slut:</p>
 		</div>
-		<div class="row col-lg-12">
-			<p>Arrangementet finder sted på addressen <?php echo $row['Location'] ?> og vil vare fra den 
-			<?php echo date("d M Y - H:i:s", $row['StartDate']) ?> til den <?php echo date("d M Y - H:i:s", $row['EndDate']) ?>.</p>
+		<div class="row col-lg-8">
+			<p><?php echo date("d M Y - H:i:s", $row['EndDate']) ?></p>
 		</div>
+		<div class="row col-lg-4">
+			<p>Adresse:</p>
+		</div>
+		<div class="row col-lg-8">
+			<p><?php echo $row['Location'] ?></p>
+		</div>
+
+		<!-- Pladser og priser -->
 		<div class="row col-lg-12">
-			<button class="button">Tilmelding</button>
+			<p><h4>Pladser og priser:</h4></p>
+		</div>
+		<div class="row col-lg-4">
+			<p>Pladser:</p>
+		</div>
+		<div class="row col-lg-8">
+			<p>On the way ...<?php //echo $row['Location'] ?></p>
+		</div>
+		<div class="row col-lg-4">
+			<p>Information om billetpriser er på vej ...</p>
+		</div>
+		<div class="row col-lg-8">
+			<p>On the way ...<?php //echo $row['Location'] ?></p>
 		</div>
 	</div>
+
 	<div class="col-lg-6 row">
 		<div class="row col-lg-12">
-			<?php echo '<img class="img-responsive" src="Images/Sponsore/'.$row['Poster'] . '">' ?>
-			<br>
-			<br>
+			<?php if ($row['Poster'] == null || $row['Poster'] == "") {
+				echo '<img class="img-responsive" src="Images/EventPoster/noposter.png">';
+			}else{
+				echo '<img class="img-responsive" src="Images/EventPoster/'.$row['Poster'] . '">';
+			} ?>
 		</div>
 	</div>
 </div>
