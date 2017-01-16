e19f5341673b8fbd6915843c81a49492340f766a
Rosenheim <patrickrosenheim@gmail.com>
2017-01-16 14:58:45 +0100

    [WIP] Building new event viewer.

--- Include/Event/Event.php
+++ Include/Event/Event.php
@@ -1,22 +1,22 @@
 <?php
   # Latest event - Get the ID.
-  $event = $db_conn->query("SELECT Event.EventID FROM Event ORDER BY EventID DESC LIMIT 1");
+  $event = $db_conn->query("SELECT Event.Title, Event.EventID, Event.Poster, Event.StartDate, Event.EndDate, Event.Location, Event.Network, Event.SeatsOpen, Event.Seatmap, Event.Rules FROM Event ORDER BY EventID DESC LIMIT 1");
   if( $event -> num_rows ) { $eventrows = $event->fetch_assoc(); }
-  # Member price info ASC
-  $result = $db_conn->query("SELECT e.Title, e.Poster, e.StartDate, e.EndDate, e.Location, e.Network, e.SeatsOpen, e.Seatmap, e.Rules, tp.StartTime, tp.EndTime, tp.Price FROM Event as e INNER JOIN TicketPrices as tp ON e.EventID = tp.EventID WHERE e.EventID = " . $eventrows["EventID"] . " AND tp.Type = 'Member' ORDER BY tp.EndTime ASC LIMIT 0, 1" );
-  if( $result -> num_rows ) { $row = $result->fetch_assoc(); }
-  # Nonmamber price info ASC
-  $result2 = $db_conn->query("SELECT e.Title, e.Poster, e.StartDate, e.EndDate, e.Location, e.Network, e.SeatsOpen, e.Seatmap, e.Rules, tp.StartTime, tp.EndTime, tp.Price FROM Event as e INNER JOIN TicketPrices as tp ON e.EventID = tp.EventID WHERE e.EventID = " . $eventrows["EventID"] . " AND tp.Type = 'Nonmember' ORDER BY tp.StartTime ASC LIMIT 0, 1");
-  if( $result2 -> num_rows ) { $row2 = $result2->fetch_assoc(); }
-  # Member price info DESC
-  $result3 = $db_conn->query("SELECT e.Title, e.Poster, e.StartDate, e.EndDate, e.Location, e.Network, e.SeatsOpen, e.Seatmap, e.Rules, tp.StartTime, tp.EndTime, tp.Price FROM Event as e INNER JOIN TicketPrices as tp ON e.EventID = tp.EventID WHERE e.EventID = " . $eventrows["EventID"] . " AND tp.Type = 'Member' ORDER BY tp.EndTime DESC LIMIT 0, 1");
-  if( $result3 -> num_rows ) { $row3 = $result3->fetch_assoc(); }
-  # Nonmamber price info DESC
-  $result4 = $db_conn->query("SELECT e.Title, e.Poster, e.StartDate, e.EndDate, e.Location, e.Network, e.SeatsOpen, e.Seatmap, e.Rules, tp.StartTime, tp.EndTime, tp.Price FROM Event as e INNER JOIN TicketPrices as tp ON e.EventID = tp.EventID WHERE e.EventID = " . $eventrows["EventID"] . " AND tp.Type = 'Nonmember' ORDER BY tp.EndTime DESC LIMIT 0, 1");
-  if( $result4 -> num_rows ) { $row4 = $result4->fetch_assoc(); }
-  # Supplement price info ASC
+  # Member price info
+  $SqlPricesMember = $db_conn->query("SELECT * FROM TicketPrices WHERE TicketPrices.EventID = " . $eventrows["EventID"] . " AND TicketPrices.Type = 'Member'" );
+  if( $SqlPricesMember -> num_rows ) { $pricesMember = $SqlPricesMember->fetch_assoc(); }
+  # Nonmamber price info
+  $SqlPricesNonMember = $db_conn->query("SELECT tp.StartTime, tp.EndTime, tp.Price FROM Event as e INNER JOIN TicketPrices as tp ON e.EventID = tp.EventID WHERE e.EventID = " . $eventrows["EventID"] . " AND tp.Type = 'Nonmember' ORDER BY tp.StartTime ASC LIMIT 0, 1");
+  if( $SqlPricesNonMember -> num_rows ) { $pricesNonMember = $SqlPricesNonMember->fetch_assoc(); }
+  # Supplement price info
   $result5 = $db_conn->query("SELECT tp.StartTime, tp.EndTime, tp.Price FROM Event as e INNER JOIN TicketPrices as tp	ON e.EventID = tp.EventID WHERE e.EventID = " . $eventrows["EventID"] . " AND tp.Type = 'Supplement' ORDER BY tp.StartTime ASC LIMIT 0, 1");
   if( $result5 -> num_rows ) { $row5 = $result5->fetch_assoc(); }
+
+  if (true) {
+    echo "<pre>";
+    var_dump($SqlPricesMember);
+    echo "</pre>";
+  }
 ?>
 
 <div class="col-lg-12 hlpf_contentbox"> <!-- Ret class til-->
@@ -35,43 +35,70 @@
       </thead>
       <tbody>
         <tr>
-          <td><?php echo $row['Title'] ?></td>
-          <td><?php echo date("d M Y - H:i:s", $row['StartDate']) ?></td>
-          <td><?php echo date("d M Y - H:i:s", $row['EndDate']) ?></td>
-          <td><?php echo $row['Location'] ?></td>
+          <td><?php echo $eventrows['Title'] ?></td>
+          <td><?php echo date("d M Y - H:i:s", $eventrows['StartDate']) ?></td>
+          <td><?php echo date("d M Y - H:i:s", $eventrows['EndDate']) ?></td>
+          <td><?php echo $eventrows['Location'] ?></td>
         </tr>
       </tbody>
     </table>
 
     <!-- Pladser-->
-    <div class="row col-lg-12">
-      <h4>Pladser</h4>
-    </div>
+    <h4>Pladser</h4>
     <table class="table table-striped">
     <tr>
       <td>Pladser</td>
       <td>
         <img class="img-responsive" src="Images/Temp/seatmaphere.png">
-        <?php if ($row['Seatmap'] == null || $row['Seatmap'] == "") {
+        <?php if ($eventrows['Seatmap'] == null || $eventrows['Seatmap'] == "") {
           echo "Ingen information tilgængelig.";
         } else {
-          echo $row['Seatmap'];
+          echo $eventrows['Seatmap'];
         } ?>
       </td>
     </tr>
     </table>
     <!-- Tilmelding og priser -->
-    <div class="row col-lg-12">
-      <h4>Tilmelding og priser</h4>
-    </div>
+    <h4>Tilmelding og priser</h4>
+    <table class="table table-responsive table-striped table-hover">
+    <thead>
+      <tr>
+        <th><!-- Empty field. --></th>
+        <?php
+          for ($i=0; $i < $SqlPricesMember -> num_rows; $i++) {
+            echo "<th>" . $TEXT_HERE . "</th>";
+          }
+        ?>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th>Medlem</th>
+        <?php
+          for ($i=0; $i < $pricerowscounterthingy; $i++) {
+            echo "<th>" . $TEXT_HERE . "</th>";
+          }
+        ?>
+      </tr>
+      <tr>
+        <th>Ikke medlem</th>
+        <?php
+          for ($i=0; $i < $pricerowscounterthingy; $i++) {
+            echo "<th>" . $TEXT_HERE . "</th>";
+          }
+        ?>
+      </tr>
+    </tbody>
+    </table>
+
     <table class="table table-striped">
       <tr>
         <th>Tilmelding for medlemmer - Før prisstigning</th>
         <th>Pris</th>
       </tr>
       <tr>
-        <td><?php echo date("d M Y - H:i:s", $row['StartTime']) ?></td>
-        <td><?php echo $row['Price'] ?> DKK</td>
+        <td><?php echo date("d M Y - H:i:s", $pricesMember['StartTime']) ?></td>
+        <td><?php echo $pricesMember['Price'] ?> DKK</td>
       </tr>
       <tr>
         <th>Tilmelding for medlemmer - Efter prisstigning</th>
@@ -111,13 +138,11 @@
     </table>
 
     <!-- Netværk -->
-    <div class="row col-lg-12">
-      <h4>Netværk</h4>
-    </div>
+    <h4>Netværk</h4>
     <table class="table table-striped">
       <tr>
         <td>Lokalnetværk</td>
-        <td><?php echo $row['Network'] ?></td>
+        <td><?php echo $eventrows['Network'] ?></td>
       </tr>
       <tr>
         <td>Internet</td>
@@ -127,33 +152,32 @@
 
     <!-- Faciliteter -->
     <!--
-		<div class="row col-lg-12">
-			<p><h4>Faciliteter:</h4></p>
-		</div>
-		<div class="row col-lg-4">
-			<p>Soverum:</p>
-		</div>
-		<div class="row col-lg-8">
-			<p>On the way ...<?php //echo $row['Location'] ?></p>
-		</div>
-		<div class="row col-lg-4">
-			<p>Bad:</p>
-		</div>
-		<div class="row col-lg-8">
-			<p>On the way ...<?php //echo $row['Location'] ?></p>
-		</div>
-		<div class="row col-lg-4">
-			<p>Kiosk:</p>
-		</div>
-		<div class="row col-lg-8">
-			<p>On the way ...<?php //echo $row['Location'] ?></p>
-		</div>
-		-->
+    <div class="row col-lg-12">
+      <p><h4>Faciliteter:</h4></p>
+    </div>
+    <div class="row col-lg-4">
+      <p>Soverum:</p>
+    </div>
+    <div class="row col-lg-8">
+      <p>On the way ...<?php //echo $row['Location'] ?></p>
+    </div>
+    <div class="row col-lg-4">
+      <p>Bad:</p>
+    </div>
+    <div class="row col-lg-8">
+      <p>On the way ...<?php //echo $row['Location'] ?></p>
+    </div>
+    <div class="row col-lg-4">
+      <p>Kiosk:</p>
+    </div>
+    <div class="row col-lg-8">
+      <p>On the way ...<?php //echo $row['Location'] ?></p>
+    </div>
+    -->
 
     <!-- Regler -->
-    <div class="row col-lg-12">
-      <h4>Regler for arrangementet kan ses <a href="?page=<?php echo $row['Rules'] ?>"><i>her</i></a></h4>
-    </div>
+    <h4>Regler for arrangementet kan ses <a href="?page=<?php echo $eventrows['Rules'] ?>"><i>her</i></a></h4>
+    <hr>
     <!--
 		<div class="row col-lg-4">
 			<p>Rygning tilladt:</p>
@@ -170,28 +194,22 @@
 		-->
 
     <!-- Arrangør -->
-    <div class="row col-lg-12">
-      <h4>Arrangør</h4>
-    </div>
-    <div class="row col-lg-12">
-      <p>
-        HLParty arrangeres af foreningen Hovedstadens LanParty Forening. Foreningen er en folkeoplysende forening, godkendt i Hillerød kommune. Foreningens formål er (uddrag af vedtægter):
-        <br>
-        <br>
-        "Foreningens formål er at samle unge mennesker, primært i hovedstadsområdet, med interesse for computere og IT, for derved at medvirke til at styrke medlemmernes sociale kompetencer, skabe kontakt på tværs af kommunegrænser, etnicitet, køn og alder og styrke medlemmernes almennyttige IT kundskaber til glæde for den enkelte, såvel som for samfundet."
-        <br>
-        <br>
-        Overskud fra et arrangement går til drift af foreningen (f.eks. webhotel, vedligeholdelse og nyinkøb af servere, switche, netværkskabler mv.), samt til at sikre fremtidige arrangementer.
-      </p>
-    </div>
-    <div class="row col-lg-12">
-      <p>Læs mere om foreningen bag HLParty på adressen <a href="https://hlpf.dk" target="_blank">https://hlpf.dk</a>.</p>
-    </div>
+    <h4>Arrangør</h4>
+    <p>
+      HLParty arrangeres af foreningen Hovedstadens LanParty Forening. Foreningen er en folkeoplysende forening, godkendt i Hillerød kommune. Foreningens formål er (uddrag af vedtægter):
+      <br>
+      <br>
+      "Foreningens formål er at samle unge mennesker, primært i hovedstadsområdet, med interesse for computere og IT, for derved at medvirke til at styrke medlemmernes sociale kompetencer, skabe kontakt på tværs af kommunegrænser, etnicitet, køn og alder og styrke medlemmernes almennyttige IT kundskaber til glæde for den enkelte, såvel som for samfundet."
+      <br>
+      <br>
+      Overskud fra et arrangement går til drift af foreningen (f.eks. webhotel, vedligeholdelse og nyinkøb af servere, switche, netværkskabler mv.), samt til at sikre fremtidige arrangementer.
+    </p>
+    <p>Læs mere om foreningen bag HLParty på adressen <a href="https://hlpf.dk" target="_blank">https://hlpf.dk</a>.</p>
   </div>
 
   <div class="col-lg-4">
     <div class="col-lg-12">
-    <?php if ($row['Poster'] == null || $row['Poster'] == "") {
+    <?php if ($eventrows['Poster'] == null || $eventrows['Poster'] == "") {
       echo '<img class="img-responsive" src="Images/EventPoster/noposter.png">';
     }else{
       echo '<img class="img-responsive" src="Images/EventPoster/'.$row['Poster'] . '">';
@@ -200,4 +218,7 @@
   </div>
 </div>
 
-<?php $result -> close(); ?>
+<?php
+
+  $event -> close();
+?>
