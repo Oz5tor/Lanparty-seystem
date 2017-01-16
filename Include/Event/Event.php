1377329c0da33113ee5a4a006cd2c5d5b32a6266
Rosenheim <patrickrosenheim@gmail.com>
2017-01-16 14:09:39 +0100

    Very clean code.
    No tabs.
    No trailing white-spaces.
    No widths.

--- Include/Event/Event.php
+++ Include/Event/Event.php
@@ -20,115 +20,113 @@
 ?>
 
 <div class="col-lg-12 hlpf_contentbox"> <!-- Ret class til-->
-	<div class="col-lg-8 row">
-		<div class="row col-lg-12">
-			<p><h2>Information</h2></p>
-		</div>
+  <div class="col-lg-8 row">
+    <h2>Information</h2>
+    <!-- Tid og sted -->
+    <h3>Tid og sted</h3>
+    <table class="table table-responsive">
+      <thead>
+        <tr>
+          <th>Navn</th>
+          <th>Start tidspunkt</th>
+          <th>Slut tidspunkt</th>
+          <th>Sted</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><?php echo $row['Title'] ?></td>
+          <td><?php echo date("d M Y - H:i:s", $row['StartDate']) ?></td>
+          <td><?php echo date("d M Y - H:i:s", $row['EndDate']) ?></td>
+          <td><?php echo $row['Location'] ?></td>
+        </tr>
+      </tbody>
+    </table>
 
-		<!-- Tid og sted -->
-		<div class="row col-lg-12">
-			<h4>Tid og sted</h4>
-		</div>
-  	<table class="table table-striped">
-  	  <tr>
-    		<td width="40%">Navn</td>
-    		<td><?php echo $row['Title'] ?></td>
-  	  </tr>
-  	  <tr>
-    		<td width="40%">Start</td>
-    		<td><?php echo date("d M Y - H:i:s", $row['StartDate']) ?></td>
-  	  </tr>
-  	  <tr>
-    		<td width="40%">Slut</td>
-    		<td><?php echo date("d M Y - H:i:s", $row['EndDate']) ?></td>
-  	  </tr>
-  	  <tr>
-    	  <td width="10%">Adresse</td>
-    		<td><?php echo $row['Location'] ?></td>
-  	  </tr>
-  	</table>
-
-		<!-- Pladser-->
-		<div class="row col-lg-12">
-			<h4>Pladser</h4>
-		</div>
+    <!-- Pladser-->
+    <div class="row col-lg-12">
+      <h4>Pladser</h4>
+    </div>
     <table class="table table-striped">
     <tr>
-		  <td width="40%">Pladser</td>
+      <td>Pladser</td>
       <td>
-        <?php if ($row['Seatmap'] == null || $row['Seatmap'] == "") { ?>
-          Ingen information tilgængelig.
-        <?php }else{
+        <img class="img-responsive" src="Images/Temp/seatmaphere.png">
+        <?php if ($row['Seatmap'] == null || $row['Seatmap'] == "") {
+          echo "Ingen information tilgængelig.";
+        } else {
           echo $row['Seatmap'];
         } ?>
       </td>
     </tr>
     </table>
-
-		<!-- Tilmelding og priser -->
-		<div class="row col-lg-12">
-			<h4>Tilmelding og priser</h4>
-		</div>
-		<table class="table table-striped">
-      <tr>
-  			<td width="70%">Tilmelding for medlemmer før prisstigning:</td><td></td>
-      </tr>
-      <tr>
-  			<td width="40%"><?php echo date("d M Y - H:i:s", $row['StartTime']) ?></td>
-        <td>Pris: <?php echo $row['Price'] ?></td>
-      </tr>
-      <tr>
-  			<td width="70%">Tilmelding for alle før prisstigning:</td><td></td>
-      </tr>
-      <tr>
-        <td width="40%"><?php echo date("d M Y - H:i:s", $row2['StartTime']) ?></td>
-        <td>Pris: <?php echo $row2['Price'] ?></td>
-      </tr>
-      <tr>
-        <td width="70%">Tilmelding for medlemmer efter prisstigning:</td><td></td>
-      </tr>
-      <tr>
-        <td width="40%"><?php echo date("d M Y - H:i:s", $row3['StartTime']) ?></td>
-        <td>Pris: <?php echo $row3['Price'] ?></td>
-      </tr>
-      <tr>
-        <td width="70%">Tilmelding for alle efter prisstigning:</td><td></td>
-      </tr>
-      <tr>
-        <td width="40%"><?php echo date("d M Y - H:i:s", $row4['StartTime']) ?></td>
-        <td>Pris: <?php echo $row4['Price'] ?></td>
-      </tr>
-      <tr>
-  			<td width="70%">Senest tilmelding for medlemmer:</td><td></td>
-      </tr>
-      <tr>
-  			<td width="70%"><?php echo date("d M Y - H:i:s", $row3['EndTime']) ?></td><td></td>
-      </tr>
-      <tr>
-  			<td width="70%">Senest tilmelding for alle:</td><td></td>
-      </tr>
-      <tr>
-  			<td width="70%"><?php echo date("d M Y - H:i:s", $row4['EndTime']) ?></td><td></td>
-      </tr>
-		</table>
-
-		<!-- Netværk -->
-		<div class="row col-lg-12">
-			<h4>Netværk:</h4>
-		</div>
+    <!-- Tilmelding og priser -->
+    <div class="row col-lg-12">
+      <h4>Tilmelding og priser</h4>
+    </div>
     <table class="table table-striped">
       <tr>
-        <td width="40%">Lokalnetværk:</td>
+        <th>Tilmelding for medlemmer - Før prisstigning</th>
+        <th>Pris</th>
+      </tr>
+      <tr>
+        <td><?php echo date("d M Y - H:i:s", $row['StartTime']) ?></td>
+        <td><?php echo $row['Price'] ?> DKK</td>
+      </tr>
+      <tr>
+        <th>Tilmelding for medlemmer - Efter prisstigning</th>
+        <th>Pris</th>
+      </tr>
+      <tr>
+        <td><?php echo date("d M Y - H:i:s", $row3['StartTime']) ?></td>
+        <td><?php echo $row3['Price'] ?> DKK</td>
+      </tr>
+      <tr>
+        <th>Tilmelding for alle - Før prisstigning</th><th>Pris</th>
+      </tr>
+      <tr>
+        <td><?php echo date("d M Y - H:i:s", $row2['StartTime']) ?></td>
+        <td><?php echo $row2['Price'] ?> DKK</td>
+      </tr>
+      <tr>
+        <th>Tilmelding for alle - Efter prisstigning</th>
+        <th>Pris</th>
+      </tr>
+      <tr>
+        <td><?php echo date("d M Y - H:i:s", $row4['StartTime']) ?></td>
+        <td><?php echo $row4['Price'] ?> DKK</td>
+      </tr>
+      <tr>
+        <td>Senest tilmelding for medlemmer</td><td></td>
+      </tr>
+      <tr>
+        <td><?php echo date("d M Y - H:i:s", $row3['EndTime']) ?></td><td></td>
+      </tr>
+      <tr>
+        <td>Senest tilmelding for alle</td><td></td>
+      </tr>
+      <tr>
+        <td><?php echo date("d M Y - H:i:s", $row4['EndTime']) ?></td><td></td>
+      </tr>
+    </table>
+
+    <!-- Netværk -->
+    <div class="row col-lg-12">
+      <h4>Netværk</h4>
+    </div>
+    <table class="table table-striped">
+      <tr>
+        <td>Lokalnetværk</td>
         <td><?php echo $row['Network'] ?></td>
       </tr>
       <tr>
-        <td width="40%">Internet:</td>
+        <td>Internet</td>
         <td>100 Mbit / 100 Mbit</td>
       </tr>
     </table>
 
-		<!-- Faciliteter -->
-		<!--
+    <!-- Faciliteter -->
+    <!--
 		<div class="row col-lg-12">
 			<p><h4>Faciliteter:</h4></p>
 		</div>
@@ -152,11 +150,11 @@
 		</div>
 		-->
 
-		<!-- Regler -->
-		<div class="row col-lg-12">
-			<h4>Regler for arrangementet kan ses <a href="?page=<?php echo $row['Rules'] ?>"><i>her</i></a></h4>
-		</div>
-		<!--
+    <!-- Regler -->
+    <div class="row col-lg-12">
+      <h4>Regler for arrangementet kan ses <a href="?page=<?php echo $row['Rules'] ?>"><i>her</i></a></h4>
+    </div>
+    <!--
 		<div class="row col-lg-4">
 			<p>Rygning tilladt:</p>
 		</div>
@@ -171,35 +169,35 @@
 		</div>
 		-->
 
-		<!-- Arrangør -->
-		<div class="row col-lg-12">
-			<h4>Arrangør</h4>
-		</div>
-		<div class="row col-lg-12">
-			<p>
-				HLParty arrangeres af foreningen Hovedstadens LanParty Forening. Foreningen er en folkeoplysende forening, godkendt i Hillerød kommune. Foreningens formål er (uddrag af vedtægter):
-				<br>
-				<br>
-				"Foreningens formål er at samle unge mennesker, primært i hovedstadsområdet, med interesse for computere og IT, for derved at medvirke til at styrke medlemmernes sociale kompetencer, skabe kontakt på tværs af kommunegrænser, etnicitet, køn og alder og styrke medlemmernes almennyttige IT kundskaber til glæde for den enkelte, såvel som for samfundet."
-				<br>
-				<br>
-				Overskud fra et arrangement går til drift af foreningen (f.eks. webhotel, vedligeholdelse og nyinkøb af servere, switche, netværkskabler mv.), samt til at sikre fremtidige arrangementer.
-			</p>
-		</div>
-		<div class="row col-lg-12">
-			<p>Læs mere om foreningen bag HLParty på adressen <a href="https://hlpf.dk" target="_blank">https://hlpf.dk</a>.</p>
-		</div>
-	</div>
+    <!-- Arrangør -->
+    <div class="row col-lg-12">
+      <h4>Arrangør</h4>
+    </div>
+    <div class="row col-lg-12">
+      <p>
+        HLParty arrangeres af foreningen Hovedstadens LanParty Forening. Foreningen er en folkeoplysende forening, godkendt i Hillerød kommune. Foreningens formål er (uddrag af vedtægter):
+        <br>
+        <br>
+        "Foreningens formål er at samle unge mennesker, primært i hovedstadsområdet, med interesse for computere og IT, for derved at medvirke til at styrke medlemmernes sociale kompetencer, skabe kontakt på tværs af kommunegrænser, etnicitet, køn og alder og styrke medlemmernes almennyttige IT kundskaber til glæde for den enkelte, såvel som for samfundet."
+        <br>
+        <br>
+        Overskud fra et arrangement går til drift af foreningen (f.eks. webhotel, vedligeholdelse og nyinkøb af servere, switche, netværkskabler mv.), samt til at sikre fremtidige arrangementer.
+      </p>
+    </div>
+    <div class="row col-lg-12">
+      <p>Læs mere om foreningen bag HLParty på adressen <a href="https://hlpf.dk" target="_blank">https://hlpf.dk</a>.</p>
+    </div>
+  </div>
 
-	<div class="col-lg-4">
-		<div class="col-lg-12">
-			<?php if ($row['Poster'] == null || $row['Poster'] == "") {
-				echo '<img class="img-responsive" src="Images/EventPoster/noposter.png">';
-			}else{
-				echo '<img class="img-responsive" src="Images/EventPoster/'.$row['Poster'] . '">';
-			} ?>
-		</div>
-	</div>
+  <div class="col-lg-4">
+    <div class="col-lg-12">
+    <?php if ($row['Poster'] == null || $row['Poster'] == "") {
+      echo '<img class="img-responsive" src="Images/EventPoster/noposter.png">';
+    }else{
+      echo '<img class="img-responsive" src="Images/EventPoster/'.$row['Poster'] . '">';
+    } ?>
+    </div>
+  </div>
 </div>
 
 <?php $result -> close(); ?>
