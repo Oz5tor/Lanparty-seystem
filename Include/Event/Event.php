df5122381745260a2767354feb707095ea4ad697
Rosenheim <patrickrosenheim@gmail.com>
2017-01-17 10:03:26 +0100

    Event-viewer clean and optimized.
    Price-viewer now shows saledates for both members and nonmembers.
    If StartTime in TicketPrices is equalt to "0", that will be displayed as
    "Door-price".

--- Include/Event/Event.php
+++ Include/Event/Event.php
@@ -3,20 +3,15 @@
   $event = $db_conn->query("SELECT Event.Title, Event.EventID, Event.Poster, Event.StartDate, Event.EndDate, Event.Location, Event.Network, Event.SeatsOpen, Event.Seatmap, Event.Rules FROM Event ORDER BY EventID DESC LIMIT 1");
   if( $event -> num_rows ) { $eventrows = $event->fetch_assoc(); }
   # Member price info
-  $SqlPricesMember = $db_conn->query("SELECT * FROM TicketPrices WHERE TicketPrices.EventID = " . $eventrows["EventID"] . " AND TicketPrices.Type = 'Member'" );
-  if( $SqlPricesMember -> num_rows ) { $pricesMember = $SqlPricesMember->fetch_assoc(); }
+  $SqlPricesMemberQuery = "SELECT * FROM TicketPrices WHERE TicketPrices.EventID = " . $eventrows["EventID"] . " AND TicketPrices.Type = 'Member' ORDER BY TicketPrices.Price ASC";
+  //if( $SqlPricesMember -> num_rows ) { $pricesMember = $SqlPricesMember->fetch_assoc(); }
   # Nonmamber price info
-  $SqlPricesNonMember = $db_conn->query("SELECT tp.StartTime, tp.EndTime, tp.Price FROM Event as e INNER JOIN TicketPrices as tp ON e.EventID = tp.EventID WHERE e.EventID = " . $eventrows["EventID"] . " AND tp.Type = 'Nonmember' ORDER BY tp.StartTime ASC LIMIT 0, 1");
-  if( $SqlPricesNonMember -> num_rows ) { $pricesNonMember = $SqlPricesNonMember->fetch_assoc(); }
+  $SqlPricesNonMemberQuery = "SELECT * FROM TicketPrices WHERE TicketPrices.EventID = " . $eventrows["EventID"] . " AND TicketPrices.Type = 'NonMember' ORDER BY TicketPrices.Price ASC";
+  //if( $SqlPricesNonMember -> num_rows ) { $pricesNonMember = $SqlPricesNonMember->fetch_assoc(); }
   # Supplement price info
-  $result5 = $db_conn->query("SELECT tp.StartTime, tp.EndTime, tp.Price FROM Event as e INNER JOIN TicketPrices as tp	ON e.EventID = tp.EventID WHERE e.EventID = " . $eventrows["EventID"] . " AND tp.Type = 'Supplement' ORDER BY tp.StartTime ASC LIMIT 0, 1");
-  if( $result5 -> num_rows ) { $row5 = $result5->fetch_assoc(); }
-
-  if (true) {
-    echo "<pre>";
-    var_dump($SqlPricesMember);
-    echo "</pre>";
-  }
+  # Food ticket query here.
+  //$result5 = $db_conn->query("SELECT tp.StartTime, tp.EndTime, tp.Price FROM Event as e INNER JOIN TicketPrices as tp	ON e.EventID = tp.EventID WHERE e.EventID = " . $eventrows["EventID"] . " AND tp.Type = 'Supplement' ORDER BY tp.StartTime ASC LIMIT 0, 1");
+  //if( $result5 -> num_rows ) { $row5 = $result5->fetch_assoc(); }
 ?>
 
 <div class="col-lg-12 hlpf_contentbox"> <!-- Ret class til-->
@@ -30,7 +25,6 @@
           <th>Navn</th>
           <th>Start tidspunkt</th>
           <th>Slut tidspunkt</th>
-          <th>Sted</th>
         </tr>
       </thead>
       <tbody>
@@ -47,7 +41,6 @@
     <h4>Pladser</h4>
     <table class="table table-striped">
     <tr>
-      <td>Pladser</td>
       <td>
         <img class="img-responsive" src="Images/Temp/seatmaphere.png">
         <?php if ($eventrows['Seatmap'] == null || $eventrows['Seatmap'] == "") {
@@ -65,8 +58,13 @@
       <tr>
         <th><!-- Empty field. --></th>
         <?php
-          for ($i=0; $i < $SqlPricesMember -> num_rows; $i++) {
-            echo "<th>" . $TEXT_HERE . "</th>";
+          $SqlPricesMember = $db_conn->query($SqlPricesMemberQuery);
+          while ($row = mysqli_fetch_array($SqlPricesMember)) {
+            if ($row["StartTime"] != 0) {
+              echo "<th>" . date("d M H:i:s",$row["StartTime"]) . "</th>";
+            } else {
+              echo "<th>Dør-pris</th>";
+            }
           }
         ?>
       </tr>
@@ -75,67 +73,41 @@
       <tr>
         <th>Medlem</th>
         <?php
-          for ($i=0; $i < $pricerowscounterthingy; $i++) {
-            echo "<th>" . $TEXT_HERE . "</th>";
+          $SqlPricesMember = $db_conn->query($SqlPricesMemberQuery);
+          while ($row = mysqli_fetch_array($SqlPricesMember)) {
+            echo "<td>" . $row["Price"] . "DKK</td>";
           }
         ?>
       </tr>
+    </tbody>
+    <thead>
+      <tr>
+        <th><!-- Empty field. --></th>
+        <?php
+          $SqlPricesNonMember = $db_conn->query($SqlPricesNonMemberQuery);
+          while ($row = mysqli_fetch_array($SqlPricesNonMember)) {
+            if ($row["StartTime"] != 0) {
+              echo "<th>" . date("d M H:i:s",$row["StartTime"]) . "</th>";
+            } else {
+              echo "<th>Dør-pris</th>";
+            }
+          }
+        ?>
+      </tr>
+    </thead>
+    <tbody>
       <tr>
         <th>Ikke medlem</th>
         <?php
-          for ($i=0; $i < $pricerowscounterthingy; $i++) {
-            echo "<th>" . $TEXT_HERE . "</th>";
+          $SqlPricesNonMember = $db_conn->query($SqlPricesNonMemberQuery);
+          while ($row = mysqli_fetch_array($SqlPricesNonMember)) {
+            echo "<td>" . $row["Price"] . "DKK</td>";
           }
         ?>
       </tr>
     </tbody>
     </table>
-
-    <table class="table table-striped">
-      <tr>
-        <th>Tilmelding for medlemmer - Før prisstigning</th>
-        <th>Pris</th>
-      </tr>
-      <tr>
-        <td><?php echo date("d M Y - H:i:s", $pricesMember['StartTime']) ?></td>
-        <td><?php echo $pricesMember['Price'] ?> DKK</td>
-      </tr>
-      <tr>
-        <th>Tilmelding for medlemmer - Efter prisstigning</th>
-        <th>Pris</th>
-      </tr>
-      <tr>
-        <td><?php echo date("d M Y - H:i:s", $row3['StartTime']) ?></td>
-        <td><?php echo $row3['Price'] ?> DKK</td>
-      </tr>
-      <tr>
-        <th>Tilmelding for alle - Før prisstigning</th><th>Pris</th>
-      </tr>
-      <tr>
-        <td><?php echo date("d M Y - H:i:s", $row2['StartTime']) ?></td>
-        <td><?php echo $row2['Price'] ?> DKK</td>
-      </tr>
-      <tr>
-        <th>Tilmelding for alle - Efter prisstigning</th>
-        <th>Pris</th>
-      </tr>
-      <tr>
-        <td><?php echo date("d M Y - H:i:s", $row4['StartTime']) ?></td>
-        <td><?php echo $row4['Price'] ?> DKK</td>
-      </tr>
-      <tr>
-        <td>Senest tilmelding for medlemmer</td><td></td>
-      </tr>
-      <tr>
-        <td><?php echo date("d M Y - H:i:s", $row3['EndTime']) ?></td><td></td>
-      </tr>
-      <tr>
-        <td>Senest tilmelding for alle</td><td></td>
-      </tr>
-      <tr>
-        <td><?php echo date("d M Y - H:i:s", $row4['EndTime']) ?></td><td></td>
-      </tr>
-    </table>
+    <p><a href="http://hlpf.dk">Klik her</a> for at blive medlem.</p>
 
     <!-- Netværk -->
     <h4>Netværk</h4>
@@ -197,11 +169,10 @@
     <h4>Arrangør</h4>
     <p>
       HLParty arrangeres af foreningen Hovedstadens LanParty Forening. Foreningen er en folkeoplysende forening, godkendt i Hillerød kommune. Foreningens formål er (uddrag af vedtægter):
-      <br>
-      <br>
-      "Foreningens formål er at samle unge mennesker, primært i hovedstadsområdet, med interesse for computere og IT, for derved at medvirke til at styrke medlemmernes sociale kompetencer, skabe kontakt på tværs af kommunegrænser, etnicitet, køn og alder og styrke medlemmernes almennyttige IT kundskaber til glæde for den enkelte, såvel som for samfundet."
-      <br>
-      <br>
+      <blockquote class="hlpf_smallerquote">
+        <i>Foreningens formål er at samle unge mennesker, primært i hovedstadsområdet, med interesse for computere og IT, for derved at medvirke til at styrke medlemmernes sociale kompetencer, skabe kontakt på tværs af kommunegrænser, etnicitet, køn og alder og styrke medlemmernes almennyttige IT kundskaber til glæde for den enkelte, såvel som for samfundet.</i>
+        <small>Hovedstadens LANParty Forening</small>
+      </blockquote>
       Overskud fra et arrangement går til drift af foreningen (f.eks. webhotel, vedligeholdelse og nyinkøb af servere, switche, netværkskabler mv.), samt til at sikre fremtidige arrangementer.
     </p>
     <p>Læs mere om foreningen bag HLParty på adressen <a href="https://hlpf.dk" target="_blank">https://hlpf.dk</a>.</p>
@@ -219,6 +190,5 @@
 </div>
 
 <?php
-
   $event -> close();
 ?>
