2d04b5e2e893819730ad403fd5bb926605208700
Tor Soya <torsoya@gmail.com>
2017-01-20 13:00:30 +0100

    NewsLetter nog found msg added

--- Include/ShowNewsLetter.php
+++ Include/ShowNewsLetter.php
@@ -4,13 +4,18 @@ if(isset($_GET['id']) && $_GET['id'] != ''){
   $LetterResult = $db_conn->query("SELECT * FROM NewsLetter WHERE LetterID = '$URLID'");
   $rowcount = $LetterResult->num_rows;
   if($rowcount < 1){
-    // fuck off
+    ?>
+    <div class="hlpf_contentbox text-center center-block" style="width:700px">
+    <h3>Det forespurgte nyhedsbrev kunne ikke findes beklager.</h3>
+    <p><a class="btn btn-primary" href="index.php">Klik her for at komme til forsiden</a></p>
+    </div>
+    <?php
   }else{
     $LetterRow = $LetterResult->fetch_assoc();
     ?>
-  <div class="hlpf_contentbox center-block" style="width:700px">
-    <?php echo $LetterRow['Body']; ?>
-  </div>
+    <div class="hlpf_contentbox center-block" style="width:700px">
+      <?php echo $LetterRow['Body']; ?>
+    </div>
   <?php
   }
   ?>
