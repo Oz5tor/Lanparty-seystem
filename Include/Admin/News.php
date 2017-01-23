3dcd00e602fd28d74a8d8dcd4c4fe6221ac384ab
Tor Soya <torsoya@gmail.com>
2017-01-23 13:08:00 +0100

    Alignment

--- Include/Admin/News.php
+++ Include/Admin/News.php
@@ -11,25 +11,25 @@ $result = $db_conn->query( "SELECT * FROM News");
 <table class="table table-striped table-hover hlpf_adminmenu">
   <thead>
     <tr>
-      <th>ID</th>
-      <th>Title</th>
-      <th>Lavet af</th>
-      <th>Sidst ændret af</th>
-      <th>Lavet den</th>
-      <th>Sidst ændret den</th>
-      <th>Online</th>
+      <th class="text-center">ID</th>
+      <th class="text-center">Title</th>
+      <th class="text-center">Lavet af</th>
+      <th class="text-center">Sidst ændret af</th>
+      <th class="text-center">Lavet den</th>
+      <th class="text-center">Sidst ændret den</th>
+      <th class="text-center">Online</th>
     </tr>
   </thead>
   <tbody>
   <?php while ($row = $result->fetch_assoc()) { ?>
     <tr>
-      <td><?php echo $row['NewsID']; ?></td>
-      <td><?php echo $row['Title']; ?></td>
-      <td><?php echo TorGetUserName($row['AuthorID'], $db_conn); ?></td>
-      <td><?php echo TorGetUserName($row['LastEditedID'], $db_conn); ?></td>
-      <td><?php echo date("d M Y", $row['CreatedDate']); ?></td>
-      <td><?php echo date("d M Y", $row['LastEditedDate']); ?></td>
-      <td><?php echo $row['Online']; ?></td>
+      <td class="text-center"><?php echo $row['NewsID']; ?></td>
+      <td class="text-center"><?php echo $row['Title']; ?></td>
+      <td class="text-center"><?php echo TorGetUserName($row['AuthorID'], $db_conn); ?></td>
+      <td class="text-center"><?php echo TorGetUserName($row['LastEditedID'], $db_conn); ?></td>
+      <td class="text-center"><?php echo date("d M Y", $row['CreatedDate']); ?></td>
+      <td class="text-center"><?php echo date("d M Y", $row['LastEditedDate']); ?></td>
+      <td class="text-center"><?php echo $row['Online']; ?></td>
     </tr>
   <?php } ?>
   </tbody>
