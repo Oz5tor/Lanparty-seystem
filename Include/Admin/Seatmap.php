4d75bc9217462b6fc7fbd15401aa70b3d7efdedb
Rosenheim <patrickrosenheim@gmail.com>
2017-01-26 13:44:18 +0100

    Update

--- Include/Admin/Seatmap.php
+++ Include/Admin/Seatmap.php
@@ -5,7 +5,7 @@ if(isset($_GET['id']) && $_GET['id'] != ''){
 <div class="col-lg-5">
   <!-- Instructions - How to make a seatmap. -->
   <h3>Instruktioner</h3>
-  <p>Lav et seatmap ved at skrive <code>a</code>, <code>c</code> og <code>_</code> i boksen til højre.</p>
+  <p>Lav et seatmap ved at skrive <code>a</code>, <code>c</code> og <code>_</code> i boksen.</p>
   <h4>Betegnelser</h4>
   <ul>
     <li><code>a</code> En plads der er tilgængelig</li>
@@ -15,9 +15,15 @@ if(isset($_GET['id']) && $_GET['id'] != ''){
   <p><small>Note: Alle linjer skal have samme længde! Fyld resten af en linje med <code>_</code> hvis det er nødvendigt.</small></p>
 </div>
 <div class="form-group col-lg-5">
-  <h3>Lav seatmap her</h3>
-  <textarea name="generate-seat-map" rows="8" cols="50"><?php // Keep this tag close to the textarea!
-    if (!empty($action)) {
+  <?php
+  if (isset($SeatmapID) AND $action == "Edit") {
+    echo "<h3>Ændre seatmap her</h3>";
+  } else {
+    echo "<h3>Lav seatmap her</h3>";
+  }
+  ?>
+  <textarea id="generate-seat-map" name="generate-seat-map" rows="8" cols="50" autofocus><?php // Keep this tag close to the textarea!
+    if ($action == "Edit" AND !empty($SeatmapID)) {
       $result = $db_conn->query("SELECT * FROM Seatmap WHERE SeatmapID = $SeatmapID");
       if (!empty($result)) {
         $row = $result->fetch_assoc();
@@ -33,11 +39,3 @@ if(isset($_GET['id']) && $_GET['id'] != ''){
     }
     ?></textarea>
 </div>
-<div class="col-lg-12">
-  <h3>Seatmap</h3>
-  <!-- Seatmap will be inserted here -->
-  <div id="seat-map"></div>
-</div>
-
-<!-- Seat-chart for  -->
-<script type="text/javascript" src="submodules/jQuery-Seat-Charts/jquery.seat-charts.min.js"></script>
