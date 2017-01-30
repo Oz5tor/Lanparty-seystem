2c5521f79ef2a2b26dfaa88ce0cefed25278a4ba
Tor Soya <torsoya@gmail.com>
2017-01-30 12:12:01 +0100

    Checkout file for Paypal function

---
+++ class/PayPalCheckout.php
@@ -0,0 +1,63 @@
+<?php
+require_once("class/PayPalConfig.php");
+use PayPal\Api\Item; 
+use PayPal\Api\Payer; 
+use PayPal\Api\Amount; 
+use PayPal\Api\Details; 
+use PayPal\Api\Payment;
+use PayPal\Api\ItemList;
+use PayPal\Api\Transaction;
+use PayPal\Api\RedirectUrls;
+
+$Payer = new Payer();
+$Payer->setPaymentMethod('paypal');
+
+$item = new Item();
+$item->setName('Kage')
+  ->setCurrency('DKK')
+  ->setQuantity(1)
+  ->setPrice(200);
+
+$itemList = new ItemList();
+$itemList->setItems([$item]);
+
+$details = new Details();
+$details->setShipping(0)
+  ->setTax(0)
+  ->setSubtotal(200);
+
+$amount = new Amount();
+$amount->setCurrency('DKK')
+  ->setTotal(200)
+  ->setDetails($details);
+
+$transaction = new Transaction();
+$transaction->setAmount($amount)
+  ->setItemList($itemList)
+  ->setDescription('Virker det ?')
+  ->setInvoiceNumber(uniqid());
+
+$redirectUrls = new RedirectUrls();
+$redirectUrls->setReturnUrl("http://localhost/Website-2017/index.php?page=Paypal2&success=true")
+  ->setCancelUrl("http://localhost/Website-2017/index.php?page=Paypa2l&success=false");
+
+$payment = new Payment();
+$payment->setIntent('sale')
+  ->setPayer($Payer)
+  ->setRedirectUrls($redirectUrls)
+  ->setTransactions([$transaction]); 
+
+try{
+  $payment->create($PaypalAPI);
+}catch (Exception $ex){
+  die($ex);
+}
+//echo '<pre>';
+//echo print_r($payment);
+//echo '</pre>';
+echo B
+
+header("Location: ". $payment->getApprovalLink());
+
+
+?>
