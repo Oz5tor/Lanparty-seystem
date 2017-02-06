78651fa6749b2ef17d87542763cfda870d3b985d
Tor Soya <torsoya@gmail.com>
2017-02-06 14:46:31 +0100

    Create event extended
    ++ Added
    Poster uplaode.
    ticket price periods.
    chosse seatamp

--- Include/Admin/NewOrEditEvent.php
+++ Include/Admin/NewOrEditEvent.php
@@ -9,31 +9,76 @@ if(isset($_GET['id'])){
 }
 
 if(isset($_POST['Save'])) {
+  
   $StartDate = strtotime($db_conn->real_escape_string($_POST['StartDate']));
   $EndDate   = strtotime($db_conn->real_escape_string($_POST['EndDate']));
-  $SeatsOpen = strtotime($db_conn->real_escape_string($_POST['SeatsOpen']));
+  $Location  = $db_conn->real_escape_string($_POST['Location']);
+  $Rules_ID  = $db_conn->real_escape_string($_POST['Rules']);
   $Title     = $db_conn->real_escape_string($_POST['Title']);
-  # Uncomment for poster
-  #$Poster           = $db_conn->real_escape_string($_POST['Poster']);
-  $Location         = $db_conn->real_escape_string($_POST['Location']);
-  $Rules_ID         = $db_conn->real_escape_string($_POST['Rules']);
+  $Pricegroups[] = $_POST['TypeList'];
+  $Pricegroups = $Pricegroups[0]; // get the inner arrays
+  $SelectedSeatmap  = $db_conn->real_escape_string($_POST['seatmap']);
+  $WanSpeed  = $db_conn->real_escape_string($_POST['wan']);
+  $LanSpeed  = $db_conn->real_escape_string($_POST['lan']);
+  $Speed = $WanSpeed.'/'.$LanSpeed;
+  # Poster
+  if($_FILES['Poster']['error'] != 4){
+  $AllowedFileTypeArray = array('jpg','png','gif');
+  $Poster = ImageUploade('Poster','Images/EventPoster',$AllowedFileTypeArray);
+  }
+  # ======
+  $PricegroupList = array();
+  $temptGroup = array();
+  $tempCount = 0;
+  foreach($Pricegroups as $groupItem){
+    
+    $group = explode('|',$groupItem);
+    
+    $temptGroup['Type'] = $group[0];
+    $temptGroup['Price'] = $group[1];
+    $temptGroup['Start'] = strtotime($group[2]);
+    $temptGroup['End'] = strtotime($group[3]);
+    $PricegroupList[$tempCount] = $temptGroup;
+    $tempCount++;
+  }
+  echo "<pre>";
+  print_r($PricegroupList);
+  echo "</pre>";
+  
   if($action == 'Edit') {
     // edit Query
-    if( $db_conn->query( " UPDATE Event 
+    /*if( $db_conn->query( " UPDATE Event 
                            SET Title = '$Title', StartDate = '$StartDate', EndDate = '$EndDate', Location = '$Location',
                                Network = '$Network', SeatsOpen = '$SeatsOpen', Rules = '$Rules_ID'
                            WHERE PageID = '$tempID'" 
-                       )){header("Location: index.php?page=Admin&subpage=Event#admin_menu");}
+                       )){header("Location: index.php?page=Admin&subpage=Event#admin_menu");}*/
   } else {
     // Create Query
-    if($db_conn->query("INSERT INTO Event (Title,StartDate,EndDate,Location,SeatsOpen,Rules)
-                        VALUES ('$Title', '$StartDate', '$EndDate', '$Location', '$SeatsOpen', '$Rules_ID')")){
+    if($db_conn->query("INSERT INTO Event (Title,StartDate,EndDate,Location,Rules, Network, Seatmap, Poster)
+                        VALUES ('$Title', '$StartDate', '$EndDate', '$Location', '$Rules_ID','$Speed','$SelectedSeatmap','$Poster')")){
+      
+      $TempEventIDResult = $db_conn->query("Select EventID FROM Event ORDER BY EventID DESC LIMIT 1");
+      $TempEventID = $TempEventIDResult->fetch_assoc();
+      $TempEventID = $TempEventID['EventID'];
+      $tempCount = 0;
+      foreach($PricegroupList as $item){
+        
+        $Type   = $item['Type'];
+        $Price  = $item['Price'];
+        $Start  = $item['Start'];
+        $End    = $item['End'];
+        
+        $db_conn->query("INSERT INTO TicketPrices (EventID, StartTime, Type, EndTime, Price)
+                                    VALUES ('$TempEventID', '$Start', '$Type', '$End', '$Price')");
+      }
+      
+      
       header("Location: index.php?page=Admin&subpage=Event#admin_menu");
     }
   }
 }
 ?>
-<form method="post" class="form-group" action="">
+<form method="post" class="form-group" enctype="multipart/form-data" action="">
   <div class="form-group col-lg-6">
     <label class="control-label" for="Title">Title</label>
     <input required class="form-control" type="text" name="Title" id="Title" value="<?php if(isset($EventExist)){echo $row['Title'];} ?>" maxlength="50"/>
@@ -50,22 +95,51 @@ if(isset($_POST['Save'])) {
     <label class="control-label" for="Location">Lokation</label>
     <input class="form-control" required type="text" name="Location" id="Location" value="<?php if(isset($EventExist)){echo $row['Location'];} ?>" />
   </div>
-  <div class="form-group col-lg-3">
-    <label class="control-label" for="SeatsOpen">SeatsOpen</label>
-    <input class="form-control form_datetime" required type="datetime-local" name="SeatsOpen" id="SeatsOpen" value="<?php if(isset($EventExist)){echo $row['SeatsOpen'];} ?>" />
-  </div>
-  <div class="form-group col-lg-3">
-    <label class="control-label" for="Poster">Poster</label>
-    <input class="form-control" type="file" id="Poster">
+  <div class="form-group col-lg-6">
+     <label class="control-label" for="RuleSetSelect">Regelsæt (Vælg en)</label>
+    <?php 
+    $GetRuleSets = $db_conn->query(" SELECT Pages.PageID, Pages.PageTitle, Pages.LastEditedDate 
+                                           FROM Pages WHERE Pages.PageTitle like 'Reg%' ORDER BY Pages.LastEditedDate DESC");
+    ?>
+    <select name="Rules" class="form-control" id="RuleSetSelect">
+      <?php while( $RuleSets = $GetRuleSets->fetch_assoc() ) { ?>
+        <option value="<?php echo $RuleSets['PageID'] ?>"><?php echo $RuleSets['PageTitle'] . " --- Sidst ændret den " . date("d M Y", $RuleSets['LastEditedDate']); ?></option>
+      <?php } ?>
+    </select>
   </div>
   <div class="form-group col-lg-6">
-    <label class="control-label" for="Type">Tilføj billet typer</label>
+    <label class="control-label" for="">Plads Layout (Vælg en)</label>
+    <select required class="form-control" name="seatmap" id="seatmap">
+      <option></option>
+      <?php
+        $GetSeatmaps = $db_conn->query("SELECT * FROM Seatmap");
+        while($Seatmap = $GetSeatmaps->fetch_assoc()){
+          echo '<option value="'.$Seatmap['SeatmapID'].'">'.$Seatmap['Name'].'</option>';
+        }
+      ?>
+    </select>
+  </div>
+  <div class="form-group col-lg-3">
+    <label class="control-label" for="StartDate">Ineternet Hastighed</label>
+    <div class="input-group">
+      <input class="form-control" name="wan" id="wan" placeholder="1024" type="text">
+      <div class="input-group-addon">Mb</div>
+    </div>
+  </div>
+  <div class="form-group col-lg-3">
+    <label class="control-label" for="EndDate">Lokalt Netværks Hastighed</label>
+    <div class="input-group">
+      <input class="form-control" name="lan" id="lan" placeholder="100" type="text">
+      <div class="input-group-addon">Mb</div>
+    </div>
+  </div>
+  <div class="form-group col-lg-6">
     <div class="form-group">
+    <label class="control-label" for="Type">Tilføj billet typer</label>
         <select class="form-control" type="text" placeholder="Type" class="" name="region" id="Type">
         <option></option>
         <?php 
           $TTypeResult = $db_conn->query("SELECT * FROM TicketTypes");
-          
           while($TypeRow = $TTypeResult->fetch_assoc()){
             echo "<option value='".$TypeRow['Type']."'>".$TypeRow['Type']."</option>";
           }
@@ -75,21 +149,21 @@ if(isset($_POST['Save'])) {
     <div class="form-inline">
       <div class="input-group">
         <input class="form-control" type="text" placeholder="150" size="2" class="" name="region" id="TypePrice" />
-        <div class="input-group-addon">,-</div>
+        <div class="input-group-addon">,-&nbsp;</div>
       </div>
       <div class="input-group">
-      <input class="form-control form_datetime" placeholder="dd-mm-yyyy" data-date-format="dd-mm-yyyy hh:ii" type="datetime" size="21" name="region" id="TypeStart" />
+      <input class="form-control form_datetime" placeholder="dd-mm-yyyy hh:mm" data-date-format="dd-mm-yyyy hh:ii" type="datetime" size="21" name="region" id="TypeStart" />
       <div class="input-group-addon">&#x1f4c5;</div>
       </div>
       <div class="input-group">
-      <input class="form-control form_datetime" placeholder="dd-mm-yyyy" data-date-format="dd-mm-yyyy hh:ii" type="datetime" size="21" name="region" id="TypeEnd" />
+      <input class="form-control form_datetime" placeholder="dd-mm-yyyy hh:mm" data-date-format="dd-mm-yyyy hh:ii" type="datetime" size="21" name="region" id="TypeEnd" />
       <div class="input-group-addon">&#x1f4c5;</div>
       </div>
     </div>
     &nbsp;
     
     <input type="button" name="add" id="btn_AddToList" value="Tilføj" class="btn btn-success form-control" />
-    <select size="10" class="form-control" id="TypeList" multiple="multiple"></select>
+    <select size="10" class="form-control" name="TypeList[]" id="TypeList" multiple="multiple"></select>
     <input type="button" name="add" id="btn_RemoveFromList" value="Fjern" class="btn btn-danger form-control" />
     
     <script type="text/javascript">
@@ -99,7 +173,7 @@ if(isset($_POST['Save'])) {
           var price     = $('#TypePrice').val();
           var startdate = $('#TypeStart').val();
           var enddate   = $('#TypeEnd').val();
-          $('#TypeList').append('<option value="'+type+' '+price+' '+startdate+' '+enddate+'">'+type+' | '+price+',- | '+startdate+' | '+enddate+'</option>');
+          $('#TypeList').append('<option value="'+type+'|'+price+'|'+startdate+'|'+enddate+'">'+type+' | '+price+',- | '+startdate+' | '+enddate+'</option>');
           
           $('#Type').val('').focus();
           $('#TypePrice').val('');
@@ -119,19 +193,19 @@ if(isset($_POST['Save'])) {
       });
     </script>
   </div>
-  <div class="form-group col-lg-6">
-    <label for="RuleSetSelect">Regelsæt (Vælg en)</label>
-    <?php 
-    $GetRuleSets = $db_conn->query(" SELECT Pages.PageID, Pages.PageTitle, Pages.LastEditedDate 
-                                           FROM Pages WHERE Pages.PageTitle like 'Reg%' ORDER BY Pages.LastEditedDate DESC");
-    ?>
-    <select name="Rules" class="form-control" id="RuleSetSelect">
-      <?php while( $RuleSets = $GetRuleSets->fetch_assoc() ) { ?>
-        <option value="<?php echo $RuleSets['PageID'] ?>"><?php echo $RuleSets['PageTitle'] . " --- Sidst ændret den " . date("d M Y", $RuleSets['LastEditedDate']); ?></option>
-      <?php } ?>
-    </select>
-  </div>
+  <div class=" col-lg-6">
+    <label class="control-label" for="Poster">Poster</label>
+    <input class="" type="file" id="Poster" name="Poster">
+  </div>  
   <div class="form-group col-lg-12">
-    <input class="btn btn-primary" type="submit" value="Gem" name="Save" />
+    <script type="text/javascript">
+    function selectAll() 
+      { 
+        selectBox = document.getElementById("TypeList");
+        for (var i = 0; i < selectBox.options.length; i++) 
+        { selectBox.options[i].selected = true; } 
+      }
+    </script>
+    <input class="btn btn-primary" type="submit" value="Gem" name="Save" onclick="selectAll()" />
   </div>
 </form>
