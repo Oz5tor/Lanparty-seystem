c680c777bdbe82644910da99b036ef573f35ccf5
Rosenheim <patrickrosenheim@gmail.com>
2017-02-06 09:09:33 +0100

    Ready for Seatmap naming.
    Adding 90 CPL

--- Include/Admin/Seatmap/NewOrEditSeatmap.php
+++ Include/Admin/Seatmap/NewOrEditSeatmap.php
@@ -5,21 +5,22 @@ if(isset($_GET['id']) && $_GET['id'] != '') {
 }
 if(!empty($_POST)) {
   $fullString = trim(preg_replace('/\s+/', '', $_POST['generate-seat-map']));
+  $SeatmapName = $_POST['Name'];
   $width = strlen(preg_split('/\s+/', $_POST['generate-seat-map'])[0]);
-  $availableSeats = preg_match_all('/(a)/', $fullString);
-  $crewSeats = preg_match_all('/(c)/', $fullString);
+  $availableSeats = substr_count($fullStrin, "a");
+  $crewSeats = substr_count($fullStrin, "c");
 
   if(isset($action) AND $action == "Edit") {
     $query = "UPDATE hlparty.Seatmap
-        SET Seatmap.SeatString = ".$fullString.", Seatmap.Width = ".$width.",
+        SET Seatmap.SeatString = '".$fullString."', Seatmap.Width = ".$width.",
             Seatmap.Seats = ".$availableSeats.", Seatmap.CrewSeats = ".$crewSeats."
         WHERE Seatmap.SeatmapID = ".$SeatmapID.";";
   } elseif (isset($action) AND $action == "New") {
     $query = "INSERT INTO  hlparty.Seatmap (
-        Width, SeatString,
+        SeatString, Name, Width,
         Seats, CrewSeats
         ) VALUES (
-          '".$width."',  '".$fullString."',
+          '".$fullString."', '".$SeatmapName."' '".$width."',
           '".$availableSeats."',  '".$crewSeats."'
         );";
   }
@@ -45,38 +46,45 @@ if(!empty($_POST)) {
     </ul>
   </div>
 </div>
-<p><small>Note: Alle linjer skal have samme længde! Fyld resten af en linje med <code>_</code> hvis det er nødvendigt.</small></p>
+<p><small>Note: Alle linjer skal have samme længde! Fyld resten af en linje med
+<code>_</code> hvis det er nødvendigt.</small></p>
 </div>
-<div class="form-group col-lg-5">
+<div class="col-lg-5">
   <?php if (isset($action) AND $action == "Edit" AND isset($SeatmapID)){
     // If editing, show this
     echo "<h3>Ændre seatmap med ID: $SeatmapID</h3>";
   } else { echo "<h3>Nyt seatmap</h3>"; } ?>
   <form action="" method="POST">
-    <textarea id="generate-seat-map" class="hlpf_seatmap-gen" name="generate-seat-map" rows="10" cols="50" autofocus><?php // Keep this PHP-tag close to the textarea!
-    if ($action == "Edit" AND !empty($SeatmapID)) {
-      // If we are editing a seatmap...
-      $result = $db_conn->query("SELECT * FROM Seatmap WHERE SeatmapID = $SeatmapID");
-      if (!empty($result)) {
-        // Fetch the results of the seatmap
-        $row = $result->fetch_assoc();
-        // Saving this output since we will be overriding it later.
-        $SeatString = $row['SeatString'];
-        $correction = 0;
-        for ($i=$row['Width']; $i < strlen($SeatString); $i += $row['Width']) {
-          // Inset newline at [width]*[iteration]+[correction]
-          $SeatString = substr_replace($SeatString, "\n", $i+$correction, 0);
-          $correction += 1;
+    <div class="form-group">
+      <textarea id="generate-seat-map" class="form-control hlpf_seatmap-gen"
+            name="generate-seat-map" rows="10" autofocus><?php // Keep this PHP-tag close to the textarea!
+      if ($action == "Edit" AND !empty($SeatmapID)) {
+        // If we are editing a seatmap...
+        $result = $db_conn->query("SELECT * FROM Seatmap WHERE SeatmapID = $SeatmapID");
+        if (!empty($result)) {
+          // Fetch the results of the seatmap
+          $row = $result->fetch_assoc();
+          // Saving this output since we will be overriding it later.
+          $SeatString = $row['SeatString'];
+          $correction = 0;
+          for ($i=$row['Width']; $i < strlen($SeatString); $i += $row['Width']) {
+            // Inset newline at [width]*[iteration]+[correction]
+            $SeatString = substr_replace($SeatString, "\n", $i+$correction, 0);
+            $correction += 1;
+          }
+          echo $SeatString;
         }
-        echo $SeatString;
-        // Quick garbage colletion...
-        unset($correction, $SeatString, $result, $row);
       }
-    }
-    ?></textarea>
-    <br>
-    <a class="btn btn-primary" onclick="generatePreview()">Preview</a>
-    <button class="btn btn-primary" type="submit">Save</button>
+      ?></textarea>
+    </div>
+    <div class="form-group">
+      <input type="text" class="form-control" id="SeatmapName" name="SeatmapName"
+            placeholder="Navn til seatmap" value="<?php echo $row['Name'] ?>">
+    </div>
+    <div class="form-group">
+      <a class="btn btn-primary" onclick="generatePreview()">Preview</a>
+      <button class="btn btn-primary" type="submit">Save</button>
+    </div>
   </form>
 </div>
 <script src="JS/seat-charts/jquery.seat-charts.min.js"></script>
