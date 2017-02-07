f5c8a3f721c47f7d5dc97a7ffd90840a37af3657
Rosenheim <patrickrosenheim@gmail.com>
2017-02-07 10:11:36 +0100

    Favicon!

--- index.php
+++ index.php
@@ -17,7 +17,20 @@ include_once("Include/DEBUGGIN.php");
     <meta name="description" content="">
     <meta name="author" content="">
     <title>HLParty <?php if(isset($html_headder_title)){echo '- '.$html_headder_title;} ?></title>
-    <link rel="shortcut icon" href="">
+    <!-- Favicon - Update with https://realfavicongenerator.net/ -->
+    <link rel="apple-touch-icon" sizes="180x180" href="Images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="Images/favicon/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="Images/favicon/favicon-194x194.png" sizes="194x194">
+    <link rel="icon" type="image/png" href="Images/favicon/android-chrome-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="Images/favicon/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="Images/favicon/manifest.json">
+    <link rel="mask-icon" href="Images/favicon/safari-pinned-tab.svg" color="#16d13f">
+    <meta name="apple-mobile-web-app-title" content="HLParty">
+    <meta name="application-name" content="HLParty">
+    <meta name="msapplication-TileColor" content="#2060c0">
+    <meta name="msapplication-TileImage" content="Images/favicon/mstile-144x144.png">
+    <meta name="theme-color" content="#2060c0">
+    <!-- Favicon end -->
     <!-- Stylesheets -->
     <link rel="stylesheet" type="text/css" href="Style/seatmap/hlpf_seatsStyling.css">
     <!-- Font awesome -->
