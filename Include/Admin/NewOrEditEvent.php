4e6d74d23d51c403b661e7d0f21ef62ea89ba846
Tor Soya <torsoya@gmail.com>
2017-02-07 14:56:02 +0100

    Crew Added to Event creation

--- Include/Admin/NewOrEditEvent.php
+++ Include/Admin/NewOrEditEvent.php
@@ -17,6 +17,8 @@ if(isset($_POST['Save'])) {
   $Title     = $db_conn->real_escape_string($_POST['Title']);
   $Pricegroups[] = $_POST['TypeList'];
   $Pricegroups = $Pricegroups[0]; // get the inner arrays
+  $PostCrewgroups[] = $_POST['CrewList'];
+  $PostCrewgroups = $PostCrewgroups[0]; // get the inner arrays
   $SelectedSeatmap  = $db_conn->real_escape_string($_POST['seatmap']);
   $WanSpeed  = $db_conn->real_escape_string($_POST['wan']);
   $LanSpeed  = $db_conn->real_escape_string($_POST['lan']);
@@ -41,13 +43,23 @@ if(isset($_POST['Save'])) {
     $PricegroupList[$tempCount] = $temptGroup;
     $tempCount++;
   }
-  echo "<pre>";
-  print_r($PricegroupList);
-  echo "</pre>";
+  // ========== Crew List run thrug =============
+  $CrewgroupList = array();
+  $temptGroup = array();
+  $tempCount = 0; // zerro for reuse
+  foreach($PostCrewgroups as $groupItem){
+    
+    $group = explode('|',$groupItem);
+    
+    $temptGroup['User'] = $group[0];
+    $temptGroup['Group'] = $group[1];
+    $CrewgroupList[$tempCount] = $temptGroup;
+    $tempCount++;
+  }
   
   if($action == 'Edit') {
     // edit Query
-    /*if( $db_conn->query( " UPDATE Event 
+    if( $db_conn->query( " UPDATE Event 
                            SET Title = '$Title', StartDate = '$StartDate', EndDate = '$EndDate', Location = '$Location',
                                Network = '$Network', SeatsOpen = '$SeatsOpen', Rules = '$Rules_ID'
                            WHERE PageID = '$tempID'" 
@@ -72,8 +84,17 @@ if(isset($_POST['Save'])) {
                                     VALUES ('$TempEventID', '$Start', '$Type', '$End', '$Price')");
       }
       
+      foreach($CrewgroupList as $item){
+        $Username   = $item['User'];
+        $CrewGroup  = $item['Group'];
+        
+        $db_conn->query("INSERT INTO Crew (EventID, Username, GroupID)
+                                    VALUES ('$TempEventID', '$Username', '$CrewGroup')");
+        
+      }
       
-      header("Location: index.php?page=Admin&subpage=Event#admin_menu");
+      
+      //header("Location: index.php?page=Admin&subpage=Event#admin_menu");
     }
   }
 }
@@ -85,11 +106,13 @@ if(isset($_POST['Save'])) {
   </div>
   <div class="form-group col-lg-3">
     <label class="control-label" for="StartDate">Start Dato</label>
-    <input class="form-control form_datetime" required type="datetime-local" name="StartDate" id="StartDate" value="<?php if(isset($EventExist)){echo $row['StartDate'];} ?>" />
+    <input class="form-control form_datetime" required type="datetime-local" name="StartDate" id="StartDate" 
+           value="<?php if(isset($EventExist)){echo date("d-m-Y H:i", $row['StartDate']);} ?>" />
   </div>
   <div class="form-group col-lg-3">
     <label class="control-label" for="EndDate">Slut Dato</label>
-    <input class="form-control form_datetime" required type="datetime-local" name="EndDate" id="EndDate" value="<?php if(isset($EventExist)){echo $row['EndDate'];} ?>" />
+    <input class="form-control form_datetime" required type="datetime-local" name="EndDate" id="EndDate"
+           value="<?php if(isset($EventExist)){echo date("d-m-Y H:i", $row['EndDate']);} ?>" />
   </div>
   <div class="form-group col-lg-6">
     <label class="control-label" for="Location">Lokation</label>
@@ -98,12 +121,15 @@ if(isset($_POST['Save'])) {
   <div class="form-group col-lg-6">
      <label class="control-label" for="RuleSetSelect">Regelsæt (Vælg en)</label>
     <?php 
-    $GetRuleSets = $db_conn->query(" SELECT Pages.PageID, Pages.PageTitle, Pages.LastEditedDate 
+    $GetRuleSets = $db_conn->query(" SELECT Pages.PageID, Pages.PageTitle AS Title, Pages.LastEditedDate 
                                            FROM Pages WHERE Pages.PageTitle like 'Reg%' ORDER BY Pages.LastEditedDate DESC");
     ?>
-    <select name="Rules" class="form-control" id="RuleSetSelect">
+    <select required name="Rules" class="form-control" id="RuleSetSelect">
+      <option>Vælg Reglsæt</option>
       <?php while( $RuleSets = $GetRuleSets->fetch_assoc() ) { ?>
-        <option value="<?php echo $RuleSets['PageID'] ?>"><?php echo $RuleSets['PageTitle'] . " --- Sidst ændret den " . date("d M Y", $RuleSets['LastEditedDate']); ?></option>
+        <option value="<?php echo $RuleSets['PageID']; ?>" <?php if(isset($EventExist) && $RuleSets['PageID'] == $row['Rules']){echo 'selected';} ?>>
+          <?php echo $RuleSets['Title'] . " --- Sidst ændret den " . date("d M Y", $RuleSets['LastEditedDate']); ?>
+        </option>
       <?php } ?>
     </select>
   </div>
@@ -114,7 +140,11 @@ if(isset($_POST['Save'])) {
       <?php
         $GetSeatmaps = $db_conn->query("SELECT * FROM Seatmap");
         while($Seatmap = $GetSeatmaps->fetch_assoc()){
-          echo '<option value="'.$Seatmap['SeatmapID'].'">'.$Seatmap['Name'].'</option>';
+        ?>
+          <option <?php if($Seatmap['SeatmapID'] == $row['Seatmap'] && isset($EventExist)) {echo 'selected';} ?> value="<?php echo $Seatmap['SeatmapID']; ?>">
+            <?php echo $Seatmap['Name'];?>
+          </option>
+      <?php
         }
       ?>
     </select>
@@ -122,21 +152,23 @@ if(isset($_POST['Save'])) {
   <div class="form-group col-lg-3">
     <label class="control-label" for="StartDate">Ineternet Hastighed</label>
     <div class="input-group">
-      <input class="form-control" name="wan" id="wan" placeholder="1024" type="text">
+      <input class="form-control" name="wan" required id="wan" placeholder="1024" type="text">
       <div class="input-group-addon">Mb</div>
     </div>
   </div>
   <div class="form-group col-lg-3">
     <label class="control-label" for="EndDate">Lokalt Netværks Hastighed</label>
     <div class="input-group">
-      <input class="form-control" name="lan" id="lan" placeholder="100" type="text">
+      <input class="form-control" name="lan" required id="lan" placeholder="100" type="text">
       <div class="input-group-addon">Mb</div>
     </div>
   </div>
+  
+  <!-- ================== Create Price Groups ======================== -->
   <div class="form-group col-lg-6">
     <div class="form-group">
     <label class="control-label" for="Type">Tilføj billet typer</label>
-        <select class="form-control" type="text" placeholder="Type" class="" name="region" id="Type">
+        <select <?php  ?> class="form-control" type="text" placeholder="Type" class="" name="region" id="Type">
         <option></option>
         <?php 
           $TTypeResult = $db_conn->query("SELECT * FROM TicketTypes");
@@ -148,7 +180,7 @@ if(isset($_POST['Save'])) {
     </div>
     <div class="form-inline">
       <div class="input-group">
-        <input class="form-control" type="text" placeholder="150" size="2" class="" name="region" id="TypePrice" />
+        <input class="form-control" type="text" pattern="[0-9]{4}" placeholder="150" size="2" class="" name="region" id="TypePrice" />
         <div class="input-group-addon">,-&nbsp;</div>
       </div>
       <div class="input-group">
@@ -162,9 +194,9 @@ if(isset($_POST['Save'])) {
     </div>
     &nbsp;
     
-    <input type="button" name="add" id="btn_AddToList" value="Tilføj" class="btn btn-success form-control" />
-    <select size="10" class="form-control" name="TypeList[]" id="TypeList" multiple="multiple"></select>
-    <input type="button" name="add" id="btn_RemoveFromList" value="Fjern" class="btn btn-danger form-control" />
+    <input type="button" name="add" id="btn_AddToList" value="Tilføj Pris Periode" class="btn btn-success form-control" />
+    <select size="5" class="form-control" name="TypeList[]" id="TypeList" multiple="multiple"></select>
+    <input type="button" name="add" id="btn_RemoveFromList" value="Fjern Pris Periode(r)" class="btn btn-danger form-control" />
     
     <script type="text/javascript">
       $(function(){
@@ -173,13 +205,15 @@ if(isset($_POST['Save'])) {
           var price     = $('#TypePrice').val();
           var startdate = $('#TypeStart').val();
           var enddate   = $('#TypeEnd').val();
-          $('#TypeList').append('<option value="'+type+'|'+price+'|'+startdate+'|'+enddate+'">'+type+' | '+price+',- | '+startdate+' | '+enddate+'</option>');
-          
-          $('#Type').val('').focus();
-          $('#TypePrice').val('');
-          $('#TypeStart').val('');
-          $('#TypeEnd').val('');
-          
+          if(type != "" && price >= 0 && startdate != "" && enddate != ""){
+            // add the price group to the list
+            $('#TypeList').append('<option value="'+type+'|'+price+'|'+startdate+'|'+enddate+'">'+type+' | '+price+',- | '+startdate+' | '+enddate+'</option>');
+            // zero the used fields
+            $('#Type').val('').focus();
+            $('#TypePrice').val('');
+            $('#TypeStart').val('');
+            $('#TypeEnd').val('');
+          }
         });
 
         $('#btn_RemoveFromList').click(function(){
@@ -193,15 +227,86 @@ if(isset($_POST['Save'])) {
       });
     </script>
   </div>
+  <!-- =================================================================== -->
   <div class=" col-lg-6">
     <label class="control-label" for="Poster">Poster</label>
     <input class="" type="file" id="Poster" name="Poster">
-  </div>  
+    &nbsp;
+  </div>
+  <!-- =================== List of useres there can become crew ======================= -->
+  <div class=" col-lg-3">
+    <label class="control-label" for="Person">Crew</label>
+    <input class="form-control" List="Admins" type="text" id="Person" name="Person">
+    <datalist id="Admins">
+      <?php
+        $GetAdmins = $db_conn->query("SELECT UserID, Username FROM Users WHERE Admin = '1'");
+        while($AdminRows = $GetAdmins->fetch_assoc()){
+        ?>
+      <option  value="<?php echo $AdminRows['Username']; ?>"><?php echo $AdminRows['Username']; ?></option>
+        <?php
+        }
+      ?>
+    </datalist>
+  </div>
+  <!-- ================= Role list ========================= -->
+  <div class=" col-lg-3">
+    <label class="control-label" for="Poster">Rolle</label>
+    <select class="form-control" type="text" id="Role" name="Role">
+      <option></option>
+      <?php 
+        $GetCrewGroups = $db_conn->query("SELECT * FROM CrewGroups ORDER BY Sort ASC");
+      ?>
+        <?php
+        while($CrewGroups = $GetCrewGroups->fetch_assoc()){
+          echo '<option value="'.$CrewGroups['GroupID'].'">'.$CrewGroups['GroupTitle'].'</option>';
+        }
+        ?>
+    </select>
+  </div>
+  <!-- ================== Crew List ======================== -->
+  <div class=" col-lg-6">
+    <input type="button" name="add" id="btn_AddToCrew" value="Tilføj Til Crew Listen" class="btn btn-success form-control" />
+    <select size="5" class="form-control" name="CrewList[]" id="CrewList" multiple="multiple"></select>
+    <input type="button" name="add" id="btn_RemoveFromCrew" value="Fjern Fra Crew Listen" class="btn btn-danger form-control" />
+  </div>
+  <!-- ========================================== -->
+  <script type="text/javascript">
+      $(function(){
+        $("#btn_AddToCrew").click(function(){
+          var user    = $('#Person').val();
+          var titlevalue   = $('#Role').val();
+          var title   = $('#Role option:selected').html();
+          
+          if(user != "" && titlevalue >= 0){
+            // add the price group to the list
+            $('#CrewList').append('<option value="'+user+'|'+titlevalue+'">'+user+' | '+title+'</option>');
+            // zero the used fields
+            $('#Person').val('').focus();
+            $('#Role').val('');
+          }
+        });
+
+        $('#btn_RemoveFromCrew').click(function(){
+          var cr = confirm('Er du sikker på du vil fjerne de valgte fra listen?');
+          if(cr == true){
+           $('#CrewList > option:selected').each(function(){
+            $(this).remove();
+          }) 
+          }
+        });
+      });
+    </script>
+  <!-- ========================================== -->
+    
   <div class="form-group col-lg-12">
     <script type="text/javascript">
     function selectAll() 
       { 
         selectBox = document.getElementById("TypeList");
+        for (var i = 0; i < selectBox.options.length; i++) 
+        { selectBox.options[i].selected = true; } 
+        
+        selectBox = document.getElementById("CrewList");
         for (var i = 0; i < selectBox.options.length; i++) 
         { selectBox.options[i].selected = true; } 
       }
