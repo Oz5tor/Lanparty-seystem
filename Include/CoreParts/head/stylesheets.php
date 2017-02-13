4b1a3e74f71a8c369e008d2d9bb7df7f6369d7d3
Rosenheim <patrickrosenheim@gmail.com>
2017-02-13 13:11:07 +0100

    Moved html header to different files.

---
+++ Include/CoreParts/head/stylesheets.php
@@ -0,0 +1,17 @@
+<!-- Stylesheets -->
+<link rel="stylesheet" type="text/css" href="Style/seatmap/hlpf_seatsStyling.css">
+<!-- Font awesome -->
+<link rel="stylesheet" href="Style/font-awesome/css/font-awesome.min.css">
+<!-- Bootstrap -->
+<link rel="stylesheet" href="Style/bootstrap.min.css">
+<link rel="stylesheet" href="Style/bootstrap-theme.min.css">
+<?php
+  if($page == 'Admin'){
+?>
+<!-- Bootstrap datetimepicker -->
+<link rel="stylesheet" href="Style/bootstrap-datetimepicker.min.css">
+<?php
+  }
+?>
+<!-- Style override -->
+<link rel="stylesheet" href="Style/hlpf_main.css">
