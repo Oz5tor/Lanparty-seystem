62ef654c1eba7d64a730b44d2f7dc458d60dfe8a
Tor Soya <torsoya@gmail.com>
2017-02-27 20:14:25 +0100

    SendMail function done
    might add a feature to attach files l8ter, not neede now.

--- class/SendMail.php
+++ class/SendMail.php
@@ -1,29 +1,41 @@
 <?php
 require_once("PHPMailer/PHPMailerAutoload.php");
 
+function SendMail($to,$toname,$subject,$body,$_GLOBAL){
 $Mailer = new PHPMailer;
 
-$Mailer->IsSMTP();
-$Mailer->SMTPAuth = true;
-$Mailer->SMTPDebug = 2;
+  // connection methohs
+  $Mailer->IsSMTP();
+  $Mailer->SMTPAuth = true;
+  $Mailer->SMTPDebug = 2;
 
-$Mailer->Host       = 'mail.rosenheim.dk ';
-$Mailer->Username   = 'tor@rosenheim.dk';
-$Mailer->Password   = 'hlparty123'; // set pass before testing
-$Mailer->SMTPSecure = 'ssl';
-$Mailer->Port       = 465;
+  // Connections Rules sets
+  $Mailer->Host       = 'mail.rosenheim.dk ';
+  $Mailer->Username   = 'tor@rosenheim.dk';
+  $Mailer->Password   = 'hlparty123';
+  $Mailer->SMTPSecure = 'ssl';
+  $Mailer->Port       = 465;
 
-$Mailer->From = 'tor@rosenheim.dk';
-$Mailer->FromName = 'Tor Soya';
-$Mailer->addReplyTo('torsoya@gmail.com','Reply adress');
-$Mailer->addAddress('torsoya@gmail.com','Tor Soya');
-
-$Mailer->Subject  = "Vi tro mail virker";
-$Mailer->Body     = "HLPartys nye hjemmeside kan sende mails nu ish";
-$Mailer->AltBody  = "HLPartys nye hjemmeside kan sende mails nu ish";
-
-echo "<pre>";
-var_dump($Mailer->send());
-echo "</pre>";
+  // is this a html mail and what charset is used
+  $Mailer->isHTML(true);
+  $Mailer->CharSet = "UTF-8";
   
+  // sender and reciver informations
+  $Mailer->From = $_GLOBAL['SendMailFrom'];
+  $Mailer->FromName = $_GLOBAL['SendMailFromName'];
+  $Mailer->addReplyTo($_GLOBAL['SendMailFromName'],'Reply adress');
+  $Mailer->addAddress($to,$toname);
+
+  // the acutaly mail
+  $Mailer->Subject  = $subject;
+  $Mailer->Body     = $body;
+  $Mailer->AltBody  = strip_tags($body,'<br>'); // for none html clients
+  
+  if($Mailer->send()){
+    return true;
+  }else{
+    return false;
+  }
+}
+ //SendMail($to,$toname,$subject,$body,$_GLOBAL);
 ?>
