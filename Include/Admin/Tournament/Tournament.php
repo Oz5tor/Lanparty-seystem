9fadf3ce4722b05ab6715c8143b1675c6e5aaf16
Tor Soya <torsoya@gmail.com>
2017-03-07 11:03:28 +0100

    testing of new functions and it works :)

--- Include/Admin/Tournament/Tournament.php
+++ Include/Admin/Tournament/Tournament.php
@@ -1,33 +1,43 @@
 <?php 
-//require_once("class/challongeClass.php");
-
-#$site_subdomain = 'hlpf'; // challonges subdomain
-#$site_domain = $site_subdomain.'.challonge.com'; // totale site domain
-#$site_API_key = 'n2aigDz8ofsnCwpHSZqapJzIf84f3C5rS4tYh6iL';
-#$resource_uri = 'https://hlpf:'.$site_API_key.'@api.challonge.com/v1/tournaments.json?subdomain=hlpf';
-#$json = file_get_contents($resource_uri);
-#$data = json_decode($json);
 require_once("Include/Admin/Tournament/ChallongeClassLib.php");
 
 $apiKey = "https://hlpf:n2aigDz8ofsnCwpHSZqapJzIf84f3C5rS4tYh6iL";
-#$what = "tournaments";
-#$atributes = "?subdomain=hlpf";
-#$data = ChallongeFunctions::ChallongeShowStuff($apiKey, $what ,$atributes);
-$what = "tournaments/2927605";
+$what = "tournaments";
 $atributes = "";
 $data = ChallongeFunctions::ChallongeShowStuff($apiKey, $what ,$atributes);
+$what = "tournaments/2927605";
+$atributes = "";
+$data2 = ChallongeFunctions::ChallongeShowStuff($apiKey, $what ,$atributes);
+
+$what = "tournaments/3288580";
+$parms = array(
+  "name" => 'name Updated'
+);
+$data3 = ChallongeFunctions::ChallongeUpdateTournament($apiKey, $what ,$parms);
+
+$data4 = ChallongeFunctions::ChallongeDeleteTournament("https://hlpf:n2aigDz8ofsnCwpHSZqapJzIf84f3C5rS4tYh6iL@api.challonge.com/v1/tournaments/3289789.json");
 
 echo "<hr>";
 echo "<pre>";
-$c = 1;
 
-print_r($data->tournament);
-/*foreach($data as $i){
-  echo $i->tournament->id.'<br>';
-  $c++;
-}*/
+echo "Enkel Tunering<br>";
+print_r($data2->tournament);
 echo "<hr>";
-print_r(ChallongeFunctions::CreateTournament("NEWsite Test 016", "HLPArtyNEWsiteTest016", "hlpf", "kage", true, true, 5, ''));
+echo "Tunerings Index ID<br>";
+$c = 1;
+foreach($data as $i){
+  echo $i->tournament->id.' '.$i->tournament->name.' | ';
+  $c++;
+}
+echo 'antal tuneringer i index '.$c;
+echo "<hr>";
+echo 'tunering updated<br>';
+print_r($data3);
+echo "<hr>";
+echo "Opret tuneringer<br>";
+print_r(ChallongeFunctions::CreateTournament("NEWsite Test 011", "HLPArtyNEWsiteTest011", "hlpf", "kage", true, true, 5, ''));
+echo "<hr>";
+echo 'tunering sletted<br>';
+print_r($data4);
 echo "</pre>";
-echo $c;
 ?>
