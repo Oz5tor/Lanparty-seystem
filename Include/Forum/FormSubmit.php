429a7f6a1831c0117bf7de6397ea258f951f3af9
MarkLenti <vikingemig@hotmail.com>
2017-03-08 10:40:43 +0100

    FormSubmit for forum fix
    FormSubmit for forum fix

--- Include/Forum/FormSubmit.php
+++ Include/Forum/FormSubmit.php
@@ -3,18 +3,18 @@
   $FormAOKAY = 0;
   if($_POST['CategoryName'] == '')        {$RegErroMSG[] .='Kategori navn'; $FormAOKAY = 1;}
   if($_POST['CategoryDesc'] == '')        {$RegErroMSG[] .='Kategori beskrivelse'; $FormAOKAY = 1;}
-  $CreateTime = time();
 
-  $CategoryName  = $_POST['CategoryName'];
-  $CategoryDesc  = $_POST['CategoryDesc'];
+  $CategoryName1  = $_POST['CategoryName'];
+  $CategoryDesc1  = $_POST['CategoryDesc'];
 
   if($FormAOKAY == 0){
     // For successfully filled
     // injection prevention
     $CategoryName   = $db_conn->real_escape_string($_POST['CategoryName']);
     $CategoryDesc   = $db_conn->real_escape_string($_POST['CategoryDesc']);
+    $CreateTime = time();
 
-    if($db_conn->query("INSERT INTO ForumCategory (Name, Descrition, CreationDate) VALUES ('$CategoryName', '$CategoryDesc', $CreateTime)")){}
+    if($db_conn->query("INSERT INTO `ForumCategory` (Name, Description, CreationDate) VALUES ('$CategoryName', '$CategoryDesc', '$CreateTime')")){}
   } // if formOKAY end
 ?>
 
