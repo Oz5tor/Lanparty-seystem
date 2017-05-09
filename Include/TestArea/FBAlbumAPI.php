4e47db1318cd3caa1294e50d55afaf9d0deb7bbf
Tor Soya <torsoya@gmail.com>
2017-05-09 14:02:25 +0200

    Token durration Test on going

--- Include/TestArea/FBAlbumAPI.php
+++ Include/TestArea/FBAlbumAPI.php
@@ -2,27 +2,54 @@
 
 function GetFBIMGS($URl){
   $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, "$URl");
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_HEADER, 0);
 
-
-curl_setopt($ch, CURLOPT_URL, "$URl");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-
-$output = curl_exec($ch);
-if($output === FALSE){
-  echo "<b>CURL ERROR: </b>". curl_error($ch);
-}
-curl_close($ch);
-return json_decode ($output);
+  $output = curl_exec($ch);
+  if($output === FALSE){
+    echo "<b>CURL ERROR: </b>". curl_error($ch);
+  }
+  curl_close($ch);
+  return json_decode ($output, true);
 } // function end
 
-$json = GetFBIMGS("https://graph.facebook.com/v2.8/631109893632404?fields=photos%7Blink%2Calbum%7D&access_token=EAAYWdebE0nIBAAzWElWRG3xdgt8ncPzTGMgX2gZB3bc1MEmXtmQ40i0dSm0Imvdgdlbd7opjJc0Kop4EpsopNMm3vThWj7vc5V9wGvt4qxclaO7L5ybuNn8h0bHnFkeXBtHV0no1m74K6O4WW");
+$url = "https://graph.facebook.com/v2.9/1333760126700707?fields=photos%7Bimages%7D&access_token=1713545498907250|yPiB5gfHwm-h0ycYq9HH1kCLh9Q";
 
+// %7B = {
+// %7D = }
+$BollNP = true;
 $imgs = array();
-
-for($i = 0; $i < count($json->photos->data); $i++){
-  $imgs[] .= $json->photos->data[$i]->link;
+$cop = 0;
+$c = 1;
+while($BollNP){
+  $json = GetFBIMGS($url);  
+  if($c == 1){
+    for($i = 0; $i < count($json['photos']['data']); $i++){
+      $imgs[] .= $json['photos']['data'][$i]['images'][1]['source'];
+    }
+  }else{
+    for($i = 0; $i < count($json['data']); $i++){
+      $imgs[] .= $json['data'][$i]['images'][1]['source'];
+    }
+  }
+  if($c == 1){
+    if(isset($json['photos']['paging']['next'])){
+    $url = $json['photos']['paging']['next'];
+    $c = 2;
+    }else{$BollNP = false;}
+  }else{
+    if(isset($json['paging']['next'])){
+    $url = $json['paging']['next'];
+    }else{$BollNP = false;}
+  }
+  $cop++;
 }
+
+foreach($imgs as $i){
+  echo '<img width="100" src="'.$i.'">';
+}
+
 //echo "<pre>";
 //echo print_r($json);
 //echo "</pre>";
@@ -31,4 +58,11 @@ for($i = 0; $i < count($json->photos->data); $i++){
 echo "<pre>";
 echo print_r($imgs);
 echo "</pre>";
+echo "<pre>";
+echo print_r($json['photos']['paging']['next']);
+echo "</pre>";
+echo "<pre>";
+echo print_r($json);
+echo "</pre>";
+echo '<hr>';
 ?>
