897c1370809d997b8a6a953301feedf6b3d9d4b2
Tor Soya <torsoya@gmail.com>
2019-11-22 09:49:42 +0100

    Update global_settings.php
    Added currunt event name and Id to the gobal Info array for eaze acess.

--- Include/CoreParts/global_settings.php
+++ Include/CoreParts/global_settings.php
@@ -5,3 +5,13 @@ $result = $db_conn->query($query);
 while($row = $result->fetch_assoc()) {
   $_GLOBAL[$row['Name']] = $row['Setting'];
 }
+
+$now = time();
+
+$query2 = "Select * from Event Where StartDate >= $now LIMIT 1";
+$result2 = $db_conn->query($query2);
+$row2 = $result2->fetch_assoc();
+
+$_GLOBAL['EventID'] = $row2['EventID'];
+$_GLOBAL['EventName'] = $row2['Title'];
+$_GLOBAL['OneAllActive'] = 0;
