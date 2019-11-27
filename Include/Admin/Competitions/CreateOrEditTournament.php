c8844aa7f9943f05151b8a2a31816807c3b5133a
Tor Soya <torsoya@gmail.com>
2019-11-27 13:22:30 +0100

    Restructure of the input order + security Fix

--- Include/Admin/Competitions/CreateOrEditTournament.php
+++ Include/Admin/Competitions/CreateOrEditTournament.php
@@ -27,19 +27,25 @@ if(isset($_POST['CreateTournament'])){
   $atts .= "&tournament[url]=$URL"; # time() + EventID + Game
   $atts = str_replace(" ", "%20", $atts);
   
-  if(ChallongeFunctions::CreateTournament($atts, $key) == True){
+  
+  if(($SignUpClose <= $SignUpOpen) || ($CompStart <= $SignUpOpen)){
+    echo '<div class="alert text-center alert-warning" role="alert">Hov Tjek Lige Datoerne </div>';
+  }else if(ChallongeFunctions::CreateTournament($atts, $key) == True){
     $db_conn->query("INSERT Competitions (EventID, GameID, CompStart, SignupOpen, SignupClose, MaxSignups, TeamSize, BracketsLink, DescText, Online)
                             VALUES ('$eventID', '$Game', '$CompStart', '$SignUpOpen', '$SignUpClose','$MaxSignups','$TeamSize','$URL', '$Desc', '$Online')");  
     header("Location: Index.php?pageAdmin&subpage=Competitions#admin_menu");
   }// If Func true end
+  else{
+    echo '<div class="alert alert-info" role="alert">Tuneringen Blev ikke Oprettet</div>';
+  }
 }// Post Form End
 ?>
     <h3>Opret Tunering:</h3>
     <form action="" method="post" enctype="multipart/form-data">
       <div class="form-group col-lg-3">
           <label class="control-label" for="Game">Hvilket Spil:</label>
-          <select name="Game" class="form-control">
-            <option>Vælg Spil</option>
+          <select required name="Game" class="form-control">
+            <option value="">Vælg Spil</option>
             <?php
               $GamesReuslt = $db_conn->query("Select * From CompetitionGames");
               while ($Games = $GamesReuslt->fetch_assoc()){
@@ -53,43 +59,37 @@ if(isset($_POST['CreateTournament'])){
           </select>
       </div>
       <div class="form-group col-lg-3">
-        <label class="control-label" for="MaxSignups">Max antal Hold</label>
-        <input type="number" class="form-control" placeholder="0" id="" value=""  name="MaxSignups" required>
+        <label class="control-label" for="CompStart">Tunering Start</label>
+        <div class="input-group">
+          <input class="form-control picker" readonly placeholder="dd-mm-yyyy hh:mm" data-date-format="dd-mm-yyyy hh:ii" required type="datetime" name="CompStart"
+                 id="CompStart" value="<?php if(isset($_POST['CompStart'])){echo $_POST['CompStart'];} ?>" />
+          <div class="input-group-addon">&#x1f4c5;</div>
+        </div>
       </div>
       <div class="form-group col-lg-3">
         <label class="control-label" for="SignUpOpen">Tilmeldning Open</label>
         <div class="input-group">
-          <input class="form-control picker" readonly placeholder="dd-mm-yyyy hh:mm" data-date-format="dd-mm-yyyy hh:ii"
-                 <?php if(isset($SaleHasStarted)){echo 'disabled';} ?> required type="text" name="SignUpOpen" id="SignUpOpen"
-                 value="<?php if(isset($EventExist)){echo date("d-m-Y H:i", $row['StartDate']);} ?>" />
+          <input class="form-control picker" readonly placeholder="dd-mm-yyyy hh:mm" data-date-format="dd-mm-yyyy hh:ii" required type="text" name="SignUpOpen" id="SignUpOpen" value="<?php if(isset($_POST['SignUpOpen'])){echo $_POST['SignUpOpen'];} ?>" />
           <div class="input-group-addon">&#x1f4c5;</div>
         </div>
       </div>
       <div class="form-group col-lg-3">
         <label class="control-label" for="SignUpClose">Tilmeldning Luk</label>
         <div class="input-group">
-          <input class="form-control picker" readonly placeholder="dd-mm-yyyy hh:mm" data-date-format="dd-mm-yyyy hh:ii"
-                 <?php if(isset($SaleHasStarted)){echo 'disabled';} ?> required type="datetime" name="SignUpClose"
-                 id="SignUpClose" value="<?php if(isset($EventExist)){echo date("d-m-Y H:i", $row['EndDate']);} ?>" />
+          <input class="form-control picker" readonly placeholder="dd-mm-yyyy hh:mm" data-date-format="dd-mm-yyyy hh:ii" required type="datetime" name="SignUpClose" id="SignUpClose" value="<?php if(isset($_POST['SignUpClose'])){echo $_POST['SignUpClose'];} ?>" />
           <div class="input-group-addon">&#x1f4c5;</div>
         </div>
       </div>
       <div class="form-group col-lg-3">
-        <label class="control-label" for="CompStart">Tunering Start</label>
-        <div class="input-group">
-          <input class="form-control picker" readonly placeholder="dd-mm-yyyy hh:mm" data-date-format="dd-mm-yyyy hh:ii"
-                 <?php if(isset($SaleHasStarted)){echo 'disabled';} ?> required type="datetime" name="CompStart"
-                 id="CompStart" value="<?php if(isset($EventExist)){echo date("d-m-Y H:i", $row['EndDate']);} ?>" />
-          <div class="input-group-addon">&#x1f4c5;</div>
-        </div>
+        <label class="control-label" for="MaxSignups">Max antal Hold</label>
+        <input type="number" class="form-control" placeholder="1" id="" value="<?php if(isset($_POST['MaxSignups'])){echo $_POST['MaxSignups'];} ?>"  name="MaxSignups" required>
       </div>
       <!-- ToDo: Style checkbox til Lable on/off -->
       <div class="form-group col-lg-3">
         <label class="control-label" for="TeamSize">Max antal Spiller per hold</label>
-        <div class="input-group">
-          <!-- Bruges kun at LanCMS og bruges ikke af Challonge API  -->
-          <input class="form-control" type="number" name="TeamSize" value="1" />
-        </div>  
+        <!-- Bruges kun at LanCMS og bruges ikke af Challonge API  -->
+          <input class="form-control" type="number" placeholder="1" name="TeamSize" value="<?php if(isset($_POST['TeamSize'])){echo $_POST['TeamSize'];} ?>" />
+        
       </div>    
       <!-- ToDo: Style checkbox til Lable on/off -->
       <div class="form-group col-lg-3">
