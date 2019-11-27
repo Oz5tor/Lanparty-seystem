57371e01361adbdac9f19e78bc39b7b5349c85e0
Tor Soya <torsoya@gmail.com>
2019-11-27 13:21:27 +0100

    Minor Changes andd added Imclude for Game Creations

--- Include/Admin/Competitions/Competitions.php
+++ Include/Admin/Competitions/Competitions.php
@@ -5,21 +5,38 @@ if($action != ''){
     $URLPageID = $db_conn->real_escape_string($_GET['id']);
   }
   switch($action){
-    case 'New':
+    case 'NewT':
       $newOrEdit = true;
       break;
-    case 'Edit':
+    case 'EditT':
       $newOrEdit = true;
+      break;
+    case 'NewG':
+      $newOrEditG = true;
       break;
   }
 } // $action end
 
 if (isset($newOrEdit) && $newOrEdit != '') {
   include_once("Include/Admin/Competitions/CreateOrEditTournament.php");
-} else {
+} else if(isset($newOrEditG)){
+  include_once("Include/Admin/Competitions/CreateNewGame.php");
+}
+else {
 ?>
-<a href="?page=Admin&subpage=Competitions&action=New#admin_menu" alt="Ny Konkurrence" type="button" class="text-center btn btn-info">Opret ny konkurrence</a>
+<div class="row">
+  <div class="col-lg-6 col-sm-6 col-xs-6">
+    <a href="?page=Admin&subpage=Competitions&action=NewT#admin_menu" alt="Ny Konkurrence" type="button" class="text-center btn btn-info">Opret ny konkurrence</a>
+  </div>
+  <div class="col-lg-6 col-sm-6 col-xs-6">
+    <a href="?page=Admin&subpage=Competitions&action=NewG#admin_menu" alt="Nyt Spil" type="button" class="text-center btn btn-info">Opret nyt spil</a>
+  </div>
+</div>
+
 <hr>
+<div class="text-center alert alert-info" role="alert">
+  Der vises kun Tuneringer for aktuelt Event ( <?= $_GLOBAL['EventName']; ?> )
+</div>
 <table class="table table-striped table-condensed table-hover LanCMSadminmenu">
   <thead>
     <tr>
@@ -31,7 +48,8 @@ if (isset($newOrEdit) && $newOrEdit != '') {
       <th class="text-center">synlig</th>
       <th class="text-center">Challonge</th> <!-- Link to the Tournament on challonge -->
       <th class="text-center">Rediger</th>
-      <th class="text-center">Se</th> <!-- Local Link on site -->
+      <th class="text-center">Mere</th>
+      <!-- "mere" trigger collabseble box to see more Actions -->
     </tr>
   </thead>
   <tbody>
@@ -42,13 +60,14 @@ if (isset($newOrEdit) && $newOrEdit != '') {
     
     # Get Matching Game Name from DB
     $TempGameID = $Comps['GameID'];
-    $GameName = $db_conn->query("SELECT GameName FROM CompetitionGames WHERE GameID = '$TempGameID'");
+    $GameName = $db_conn->query("SELECT * FROM CompetitionGames WHERE GameID = '$TempGameID'");
     $GName = $GameName->fetch_assoc();
-    # Get Matching Game Name from DB END
-      
+    # Get Matching Game Image from DB END
     ?>
     <tr>
-      <td><?php echo $GName['GameName']; ?></td>
+      <td align="center">
+        <img class=" img-responsive" width="35" src="Images/games/<?= $GName['Image']; ?>"/>
+      </td>
       <td>
         <?php if(time() >= $Comps['CompStart']){ ?>
         <button class="disabled btn btn-danger">Startet</button>
@@ -80,11 +99,11 @@ if (isset($newOrEdit) && $newOrEdit != '') {
         <a href="https://challonge.com/<?= $Comps['BracketsLink']; ?>" target="_blank" class="btn btn-info">Challonge</a>
       </td>
       <td class="text-center">
-        <a href="index.php?page=Admin&subpage=Competitions&action=Edit&id=<?= $Comps['ID']; ?>#admin_menu" style="display:block;" class="btn btn-warning">Rediger</a>
+        <a href="index.php?page=Admin&subpage=Competitions&action=EditT&id=<?= $Comps['ID']; ?>#admin_menu" style="display:block;" class="btn btn-warning">Rediger</a>
       </td>
       <td>
         <a href="index.php?page=Competittion=<?= $Comps['ID']; ?>" class="btn btn-primary">
-          Se
+          Mere
         </a>
       </td>
     </tr>
