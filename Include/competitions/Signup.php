7beca51debfaac681181a67fae7281639296c150
Tor Soya <torsoya@gmail.com>
2019-12-19 14:12:23 +0100

    Update Signup.php
    DB table created, alot of code has been made.
    
    whats left:
    get the add member to the team in the html select box and make check on it before submittin.
    code the insert into DB code when form is submitted

--- Include/competitions/Signup.php
+++ Include/competitions/Signup.php
@@ -3,8 +3,10 @@ $CompID = mysqli_real_escape_string($db_conn,strip_tags($_GET['id']));
 $comps = $db_conn->query("Select * From CompetitionGames Inner Join Competitions On Competitions.GameID = CompetitionGames.GameID
                           Inner Join Event On Competitions.EventID = Event.EventID Where ID  = $CompID LIMIT 1");
 $row = $comps->fetch_assoc();
-?>
 
+$MaxTeamSize = $row['TeamSize'];
+?>
+ <!-- =========== Competittions Info -->
 <div class="row thumbnail">
   <div class="col-lg-4 LanCMSContentbox text-center">
     <img width="" class="" src="Images/games/<?= $row['Image']; ?>" />
@@ -23,8 +25,7 @@ $row = $comps->fetch_assoc();
             </b>
       </div>
       <div class="col-lg-2"></div>
-      <div class="col-lg-3">
-      </div>
+      <div class="col-lg-3"></div>
     </div>
     <hr style="clear:both;" />
     <?= $row['DescText']; ?>  
@@ -32,15 +33,93 @@ $row = $comps->fetch_assoc();
   
   <div class="col-lg-12">
     <hr />
+    <?php if ($MaxTeamSize == 1){ ?>
+    <?php }else { ?>
+    <form action="" method="post" enctype="multipart/form-data">
+    <div class="form-group form-inline col-lg-3 col-lg-offset-3">
+      <label for="HNavn">*Hold Navn: </label>
+      <input class="form-control" type="text" id="HNavn" value="" required name="HNavn">
+    </div>
+  <!-- =================== List of useres there can be added to the team ======================= -->
+  <div class=" col-lg-3">
+    <label class="control-label" for="Person">Hold Medlem:</label>
+    <input class="form-control" List="Users" type="text" id="Person" value="" name="Person">
+    <datalist id="Users">
+      <?php
+        $tempUserID = $_SESSION['UserID'];
+        $GetUsers = $db_conn->query("Select * From Tickets 
+                                      Inner Join Users On Tickets.UserID = Users.UserID 
+                                      Inner Join CompTeamsMember On Users.Username = CompTeamsMember.UserName
+                                      Inner Join CompTeams On CompTeamsMember.TeamID = CompTeams.TeamID
+                                      Where Tickets.EventID = 2 And Users.UserID Not Like $tempUserID ");
+        while($UsersRows = $GetUsers->fetch_assoc()){
+        ?>
+      <option  value="<?php echo $UsersRows['Username']; ?>"><?php echo $UsersRows['Username']; ?></option>
+        <?php
+        }
+      ?>
+    </datalist>
+  </div>
+    <!-- ================== Team Member List ======================== -->
+  <div class="row">
+  <div class=" col-lg-6 col-lg-offset-3">
+    <input type="button" name="add" id="btn_AddToCrew" value="Tilføj Til Holdet" class="btn btn-success form-control" />
     
-    <table class="table table-striped table-consensed">
-      <tr>
-        <td><label></label></td>
-        <td></td>
-      </tr>
-    </table>
-    
-    
+    <select size="5" required class="form-control" name="TeamList[]" id="TeamMembers" multiple="multiple"></select>
+      <option> </option>
+    <input type="button" name="add" id="btn_RemoveFromCrew" value="Fjern Fra Holdet" class="btn btn-danger form-control" />
+  </div>
+  </div>
+  <!-- ========================================== -->
+  <script type="text/javascript">
+      $(function(){
+        $("#btn_AddToCrew").click(function(){
+          var user = $('#Person').val();
+
+          if(user != ""){
+            // add the price group to the list
+            $('#TeamMembers').append('<option value="'+user+'">'+user+'</option>');
+            // zero the used fields
+            $('#Person').val('').focus();
+          }
+        });
+
+        $('#btn_RemoveFromCrew').click(function(){
+          var cr = confirm('Er du sikker på du vil fjerne de valgte fra listen?');
+          if(cr == true){
+           $('#TeamMembers > option:selected').each(function(){
+            $(this).remove();
+          })
+          }
+        });
+      });
+    </script>
+  <!-- ========================================== -->
+      <div class="row">
+        <div class="text-center col-lg-4 col-lg-offset-4">
+          <br>
+          
+        <script type="text/javascript">
+          function selectAll()
+            {
+              selectBox = document.getElementById("TeamMembers");
+                var SelectedNum = selectBox.options.selected.length;
+              if (SelectedNum < <?= $MaxTeamSize; ?>){
+                  confirm("Der er ikke nok Spillere på holdet");
+                  }else if (SelectedNum < <?= $MaxTeamSize; ?>){
+                    confirm("Der er for mange Spillere på holdet");
+              }else  {
+                for (var i = 0; i < selectBox.options.length; i++)
+                { selectBox.options[i].selected = true; }
+              }
+            }
+        </script>
+        <input class="btn btn-primary " type="submit" value="Tilmeld Holdet" name="Save" onclick="selectAll()" />
+          <br>&nbsp;
+        </div>
+      </div>
+    </form>
+    <?php } ?>
   </div>
   
 </div>
