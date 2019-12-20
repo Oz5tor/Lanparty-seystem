99862d73939cea8a02a8323acf9a5561476a1e19
Tor Soya <torsoya@gmail.com>
2019-12-20 13:47:04 +0100

    Update Signup.php
    added some JS check on the team select i the form, still needs improvement

--- Include/competitions/Signup.php
+++ Include/competitions/Signup.php
@@ -1,10 +1,36 @@
 <?php
+require_once("class/GetUserNameFromID.php");
 $CompID = mysqli_real_escape_string($db_conn,strip_tags($_GET['id']));
 $comps = $db_conn->query("Select * From CompetitionGames Inner Join Competitions On Competitions.GameID = CompetitionGames.GameID
                           Inner Join Event On Competitions.EventID = Event.EventID Where ID  = $CompID LIMIT 1");
 $row = $comps->fetch_assoc();
 
 $MaxTeamSize = $row['TeamSize'];
+
+# ====================================================================================================
+# ====================================================================================================
+# ====================================================================================================
+
+
+if(isset($_POST['Save'])){
+  
+  $TeamName       = $db_conn->real_escape_string($_POST['TeamName']);
+  $TeamMembers[]  = $_POST['TeamList'];
+  $TeamMembers    = $TeamMembers[0]; // get inner array
+  $PostCount = count($TeamMembers);
+  if ($PostCount < $MaxTeamSize){
+    
+  } // end of less than $MaxTeamSize
+  else if ($PostCount > $MaxTeamSize){
+    
+  } // end of Larger than $MaxTeamSize
+  else if ($PostCount == $MaxTeamSize){ // Finaly lets do something xD
+    
+  }
+  
+  
+} // end of post
+
 ?>
  <!-- =========== Competittions Info -->
 <div class="row thumbnail">
@@ -37,8 +63,8 @@ $MaxTeamSize = $row['TeamSize'];
     <?php }else { ?>
     <form action="" method="post" enctype="multipart/form-data">
     <div class="form-group form-inline col-lg-3 col-lg-offset-3">
-      <label for="HNavn">*Hold Navn: </label>
-      <input class="form-control" type="text" id="HNavn" value="" required name="HNavn">
+      <label for="TeamName">*Hold Navn: </label>
+      <input class="form-control" type="text" id="TeamName" value="" required name="TeamName">
     </div>
   <!-- =================== List of useres there can be added to the team ======================= -->
   <div class=" col-lg-3">
@@ -64,9 +90,18 @@ $MaxTeamSize = $row['TeamSize'];
   <div class="row">
   <div class=" col-lg-6 col-lg-offset-3">
     <input type="button" name="add" id="btn_AddToCrew" value="Tilføj Til Holdet" class="btn btn-success form-control" />
-    
-    <select size="5" required class="form-control" name="TeamList[]" id="TeamMembers" multiple="multiple"></select>
-      <option> </option>
+    <select size="5" required class="form-control" name="TeamList[]" id="TeamMembers" multiple="multiple">
+      <?php 
+      if(isset($TeamMembers)){
+        foreach($TeamMembers as $x){
+          echo "<option value='$x'>$x</option>";
+        }
+      }else {
+          $TempUName = TorGetUserName($_SESSION['UserID'], $db_conn);
+          echo "<option value='$TempUName'>$TempUName</option>"; 
+      }
+      ?>
+    </select>
     <input type="button" name="add" id="btn_RemoveFromCrew" value="Fjern Fra Holdet" class="btn btn-danger form-control" />
   </div>
   </div>
@@ -74,12 +109,13 @@ $MaxTeamSize = $row['TeamSize'];
   <script type="text/javascript">
       $(function(){
         $("#btn_AddToCrew").click(function(){
-          var user = $('#Person').val();
-
+          var user = $('#Person').val(); // input field
+          //var Xperson = document.getElementById("Users"); // datalist
           if(user != ""){
             // add the price group to the list
             $('#TeamMembers').append('<option value="'+user+'">'+user+'</option>');
             // zero the used fields
+            //Xperson.children[0].remove() // for the futuere where added team members get fromoved from the datalist            
             $('#Person').val('').focus();
           }
         });
@@ -103,11 +139,11 @@ $MaxTeamSize = $row['TeamSize'];
           function selectAll()
             {
               selectBox = document.getElementById("TeamMembers");
-                var SelectedNum = selectBox.options.selected.length;
-              if (SelectedNum < <?= $MaxTeamSize; ?>){
-                  confirm("Der er ikke nok Spillere på holdet");
-                  }else if (SelectedNum < <?= $MaxTeamSize; ?>){
-                    confirm("Der er for mange Spillere på holdet");
+              var x = document.getElementById("TeamMembers").length;  
+              if (x < <?= $MaxTeamSize; ?>){
+                  alert("Der er ikke nok Spillere på holdet");
+                  }else if (x > <?= $MaxTeamSize; ?>){
+                    alert("Der er for mange Spillere på holdet");
               }else  {
                 for (var i = 0; i < selectBox.options.length; i++)
                 { selectBox.options[i].selected = true; }
