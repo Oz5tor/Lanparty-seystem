323b155f6c86f396b18bffd55d1cd14f533c297a
Tor Soya <torsoya@gmail.com>
2020-03-14 22:39:44 +0100

    Make the Oneall Box fit 5 social networks

--- Include/TopHeader.php
+++ Include/TopHeader.php
@@ -2,8 +2,8 @@
   <div class="container">
     <div class="row">
       <div class="hidden-xs col-lg-4 col-md-4 col-sm-4 col-xs-12 text-center" id="LanCMSLogo"><img class="img-responsive" src="Images/logo.png"></div>
-      <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 text-center" id="LanCMScountdown"></div>
-      <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12" id="LanCMSLogin">
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 text-center" id="LanCMScountdown"></div>
+      <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12" id="LanCMSLogin">
         <?php
           if(!isset($_SESSION['UserID'])){
             if($_GLOBAL['OneAllActive'] != 1){
