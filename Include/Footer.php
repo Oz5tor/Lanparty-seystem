5ce18b618baccc07f18d4f6499f0ee855dbaf0c7
Tor Soya <torsoya@gmail.com>
2020-03-15 20:47:08 +0100

    Code cleaning

--- Include/Footer.php
+++ Include/Footer.php
@@ -1,38 +1,38 @@
 <footer class="container-fluid" id="LanCMSfooter">
-        <div class="container">
-            <div class="row">
-            <!-- Google Maps -->
-            <div class="hidden-xs col-sm-12 col-md-6">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2234.58538684431!2d12.287926209023182!3d55.9392252493907!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465240852b497295%3A0x2ae4a89f1e73f438!2sHiller%C3%B8dsholmskolen!5e0!3m2!1sda!2sdk!4v1469351469692" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <div class="row">
+        <!-- Google Maps -->
+        <div class="hidden-xs col-sm-12 col-md-6">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2234.58538684431!2d12.287926209023182!3d55.9392252493907!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465240852b497295%3A0x2ae4a89f1e73f438!2sHiller%C3%B8dsholmskolen!5e0!3m2!1sda!2sdk!4v1469351469692" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+        <!-- Generel information, kontaktinformation og sociale medier -->
+        <div class="col-sm-12 col-md-6">
+            <!-- Adresse og kontakt -->
+            <div class="col-lg-6 hidden-xs hidden-sm hidden-md" id="LanCMSfooter_contact">
+                <h2>Adresse</h2>
+                <address>
+                    Hillerødsholmskolen <br>
+                    Hillerødsholmsallé 2 <br>
+                    3400 Hillerød
+                </address>
+                <h2>Kontakt</h2>
+                <p>Bestyrelsen kan kontaktes på <a href="mailto:best@hlpf.dk">best@hlpf.dk</a>.<br> Ellers henviser vi til vores <a href="?page=Kontakt">kontaktside</a>.</p>
             </div>
-            <!-- Generel information, kontaktinformation og sociale medier -->
-            <div class="col-sm-12 col-md-6">
-                <!-- Adresse og kontakt -->
-                <div class="col-lg-6 hidden-xs hidden-sm hidden-md" id="LanCMSfooter_contact">
-                    <h2>Adresse</h2>
-                    <address>
-                        Hillerødsholmskolen <br>
-                        Hillerødsholmsallé 2 <br>
-                        3400 Hillerød
-                    </address>
-                    <h2>Kontakt</h2>
-                    <p>Bestyrelsen kan kontaktes på <a href="mailto:best@hlpf.dk">best@hlpf.dk</a>.<br> Ellers henviser vi til vores <a href="?page=Kontakt">kontaktside</a>.</p>
-                </div>
-                <!-- Generelle links -->
-                <div class="col-lg-6">
-                    <h2>Nyttige links</h2>
-                    <ul id="LanCMSfooter_links">
-                        <li><a href="#">Regler</a></li>
-                        <li><a href="#">Konkurrencer</a></li>
-                        <li><a href="#">Kantinen</a></li>
-                    </ul>
-                    <!-- Sociale links -->
-                    <h2>Social</h2>
-                    <a href="#"><span class="fa fa-facebook fa-3x"></span></a>&nbsp;
-                    <a href="#"><span class="fa fa-twitter fa-3x"></span></a>&nbsp;
-                    <a href="#"><span class="fa fa-youtube fa-3x"></span></a>
-                </div>
-            </div> <!-- outer col -->        
-        </div> <!-- row -->
-      </div> <!--  container -->
-    </footer>
+            <!-- Generelle links -->
+            <div class="col-lg-6">
+                <h2>Nyttige links</h2>
+                <ul id="LanCMSfooter_links">
+                    <li><a href="#">Regler</a></li>
+                    <li><a href="#">Konkurrencer</a></li>
+                    <li><a href="#">Kantinen</a></li>
+                </ul>
+                <!-- Sociale links -->
+                <h2>Social</h2>
+                <a href="#"><span class="fa fa-facebook fa-3x"></span></a>&nbsp;
+                <a href="#"><span class="fa fa-twitter fa-3x"></span></a>&nbsp;
+                <a href="#"><span class="fa fa-youtube fa-3x"></span></a>
+            </div>
+        </div> <!-- outer col -->        
+    </div> <!-- row -->
+</footer>
+
+
