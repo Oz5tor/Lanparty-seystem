1a019e228fe02e21caebc1591972d644e6208fd4
Tor Soya <torsoya@gmail.com>
2020-03-21 16:28:52 +0100

    NEW Datetimepicker & Bootstrap 4.x JS
    NEW datetimepicker and new bootstrap JS

--- Include/CoreParts/head/stylesheets.php
+++ Include/CoreParts/head/stylesheets.php
@@ -3,11 +3,9 @@
 <!-- Font awesome -->
 <link rel="stylesheet" href="Style/font-awesome/css/font-awesome.min.css">
 <!-- Bootstrap -->
+<link rel="stylesheet" href="style/bootstrap-datepicker.css">
 <link rel="stylesheet" href="Style/bootstrap.min.css">
 <link rel="stylesheet" href="Style/bootstrap-theme.min.css">
-
-<!-- Bootstrap datetimepicker -->
-<link rel="stylesheet" href="Style/bootstrap-datetimepicker.min.css" media="screen">
 
 <!-- Style override -->
 <link rel="stylesheet" href="Style/LanCMSmain.css">
--- Include/CoreParts/htmlBottom.php
+++ Include/CoreParts/htmlBottom.php
@@ -1,22 +1,20 @@
 <script src="JS/Bootstrap/bootstrap.min.js"></script>
+<script type="text/javascript" src="JS/moment/moment.js"></script>
+<script type="text/javascript" src="JS/Bootstrap/tempusdominus-bootstrap-4.js"></script>
 
-<script type="text/javascript" src="JS/Bootstrap/bootstrap-datetimepicker.js" charset="UTF-8"></script>
-<script type="text/javascript" src="JS/Bootstrap/bootstrap-datetimepicker.da.js" charset="UTF-8"></script>
+<!-- ====================== -->
+ <script type="text/javascript">
+    $(function() {
+  $('.datetimepicker1').datetimepicker({
+      locale: 'da',
+      format: ''
+  });
+});
+     
 
-<script type="text/javascript">
-    $('.picker').datetimepicker({
-      language:  'da',
-      weekStart: 1,
-      todayBtn:  1,
-		  autoclose: 1,
-		  todayHighlight: 1,
-		  startView: 2,
-		  forceParse: 0,
-      showMeridian: 1
-    });
 </script>
 
-<script type="text/javascript">
+<!-- <script type="text/javascript">
     $('.birthdayPicker').datetimepicker({
       language:  'da',
       startView: 4,
@@ -25,4 +23,4 @@
       format: 'dd-mm-yyyy',
       autoclose: 1,
     });
-</script>
+</script> -->
