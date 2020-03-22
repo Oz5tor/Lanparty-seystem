5e6fd4d29f792e7693f3600cfba8ee7599a9bec8
Tor Soya <torsoya@gmail.com>
2020-03-23 00:11:47 +0100

    2 datepickers sofar
    datetimepicker2 =  has seconds included

--- Include/CoreParts/htmlBottom.php
+++ Include/CoreParts/htmlBottom.php
@@ -1,18 +1,27 @@
+
 <script src="JS/Bootstrap/bootstrap.min.js"></script>
 <script type="text/javascript" src="JS/moment/moment.js"></script>
 <script type="text/javascript" src="JS/Bootstrap/tempusdominus-bootstrap-4.js"></script>
 
-<!-- ====================== -->
- <script type="text/javascript">
-    $(function() {
-  $('.datetimepicker1').datetimepicker({
-      locale: 'da',
-      format: ''
-  });
-});
-     
 
+<script type="text/javascript">
+    
+ $(function() {
+    $('.datetimepicker1').datetimepicker({
+        locale: 'da',
+        format: 'D/M/YYYY HH:mm'
+    });
+   });
+   $(function() {
+    $('.datetimepicker2').datetimepicker({
+        locale: 'da',
+        format: 'D/M/YYYY HH:mm:ss'
+    });
+   });
 </script>
+
+<!-- ====================== -->
+
 
 <!-- <script type="text/javascript">
     $('.birthdayPicker').datetimepicker({
