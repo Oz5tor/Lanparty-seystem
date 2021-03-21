9ad21be5be4559db54dcc77e70a510d711083036
Tor Soya <torsoya@gmail.com>
2021-03-21 18:38:55 +0100

    Capital S

--- Include/CoreParts/head/stylesheets.php
+++ Include/CoreParts/head/stylesheets.php
@@ -3,7 +3,7 @@
 <!-- Font awesome -->
 <link rel="stylesheet" href="Style/font-awesome/css/font-awesome.min.css">
 <!-- Bootstrap -->
-<link rel="stylesheet" href="style/bootstrap-datepicker.css">
+<link rel="stylesheet" href="Style/bootstrap-datepicker.css">
 <link rel="stylesheet" href="Style/bootstrap.min.css">
 <link rel="stylesheet" href="Style/bootstrap-theme.min.css">
 
