6bcd5397f555e03acb4607ded7b205b428dce9f8
Tor Soya <torsoya@gmail.com>
2021-04-06 20:15:28 +0200

    dateipcker funtions update

--- Include/CoreParts/htmlBottom.php
+++ Include/CoreParts/htmlBottom.php
@@ -2,27 +2,30 @@
 <script src="JS/Bootstrap/bootstrap.min.js"></script>
 <script type="text/javascript" src="JS/moment/moment.js"></script>
 <script type="text/javascript" src="JS/Bootstrap/tempusdominus-bootstrap-4.js"></script>
-
-
 <script type="text/javascript">
     
  $(function() {
     $('.datetimepicker1').datetimepicker({
         locale: 'da',
-        format: 'DD/MM/YYYY HH:mm'
+        format: 'DD/MM/YYYY HH:mm',
+        buttons: {showClose: true}
     });
    });
    $(function() {
     $('.datetimepicker2').datetimepicker({
         locale: 'da',
-        format: 'D/M/YYYY HH:mm:ss'
+        format: 'D/M/YYYY HH:mm:ss',
+        buttons: {showClose: true}
     });
    });
     $(function() {
     $('.birthday').datetimepicker({
         locale: 'da',
-        format: 'DD-MM-YYYY'
+        format: 'DD-MM-YYYY',
+        ignoreReadonly: true,
+        buttons: {showClose: true}
     });
    });
+
 </script>
 <!-- ==================== -->
