2d3b2ee87b3a6706d8a39395db8eb70e622700eb
Tor Soya <torsoya@gmail.com>
2021-04-06 21:00:19 +0200

    user box improvements

--- Include/Usermodule/Userbox.php
+++ Include/Usermodule/Userbox.php
@@ -1,16 +1,4 @@
-<div class="row">
-  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-7">
-    <ul>
-      <?php if($_SESSION['Admin'] == 1){ ?> 
-      <li><a href="?page=Admin">Administration</a></li>
-      <?php } ?>
-      <li><a href="?page=EditMyProfile">Ret min profil</a></li>
-      <li><a href="">Min billet</a></li>
-      <li><a href="?action=LogOut">Logud</a></li>
-    </ul>
-  </div>
-  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-5">
-    <?php
+<?php
     $UserID = $_SESSION['UserID'];
     if($result = $db_conn->query("SELECT * FROM Users WHERE UserID = '$UserID'")){
       if($result -> num_rows){
@@ -18,13 +6,33 @@
         $PreffereredUsername    = $row['Username'];
         $FullName               = $row['FullName'];
         $Clan                   = $row['ClanID'];
-        if($row['ProfileIMG'] == ''){ $PictureUrl = "Images/Users/nopic.png";} else { $PictureUrl = $row['ProfileIMG']; }
+        if($row['ProfileIMG'] == ''){ $PictureUrl = "Images/Users/nopic.png";} else { $PictureUrl = "Images/Users/nopic.png"; }
       }
     }
     ?>
-    
-    <img height="80" src="<?php echo $PictureUrl; ?>">
+
+<div class="row LanCMScontentbox">
+  <div class="col-lg-2 col-md-2 col-sm-2 col-xs-7 ">
+  <img height="75" src="<?php echo $PictureUrl; ?>">
+    <?php
+#    echo "Hej ".$PreffereredUsername."<br>";
+#    echo $FullName;
+#    echo $Clan;
+    ?>
   </div>
+  <div class="col-lg-9 col-md-10 col-sm-10 col-xs-7 col-lg-offset-1 ">
+      <a class="btn btn-info" style="margin-bottom: 5px;" href="">Min billet</a>
+      <a class="btn btn-primary" style="margin-bottom: 5px;" href="?page=EditMyProfile">Ret min profil</a>
+      <a class="btn btn-danger" style="margin-bottom: 5px;" href="?action=LogOut">Logud</a>
+  <?php if($_SESSION['Admin'] == 1){ ?> 
+      <a class="btn btn-warning" style="margin-bottom: 5px;" href="?page=Admin">Administration</a>
+      <?php } ?>
+    
+    
+    
+    
+    
+  </div>  
 </div>
 
 
