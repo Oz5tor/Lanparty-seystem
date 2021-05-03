590fe95b9d3591d71e83501a9ba62e03788d117a
Tor Soya <torsoya@gmail.com>
2021-05-03 21:05:26 +0200

    Refractoring of Newsletter

--- Include/Admin/NewsLetter/NewOrEditNewsLetter.php
+++ Include/Admin/NewsLetter/NewOrEditNewsLetter.php
@@ -1,57 +1,64 @@
 <?php
-require_once("class/SendMail.php");
+require_once("class/MailGunSendMail.php");
+$Sending = 0;
 if(isset($_GET['id'])){$URLID = $db_conn->real_escape_string($_GET['id']);}
 if(isset($_POST['Save'])){
-  $Title = $db_conn->real_escape_string($_POST['Title']);
+  $Title = $db_conn->real_escape_string($_POST['Title']); # = Subject
   if(isset($_POST['Send'])){$Send = $db_conn->real_escape_string($_POST['Send']);}else {$Send = '0';}
-  $Body  = $_POST['AdminTinyMCE'];
-  $Aurthor = $_SESSION['UserID'];
-  if($action == 'Edit'){
-    if($Send == 1){
-
-      $NewsResult = $db_conn->query("SELECT Users.FullName, Users.Email, Users.NewsLetter FROM Users WHERE Users.NewsLetter = 1");
-        while($NewsRow = $NewsResult->fetch_assoc()){
-          // Send mail
-          SendMail($NewsRow["Email"],$NewsRow["FullName"],$Title,$Body,$_GLOBAL);
-          //echo "<hr>";
-        }// End of Users that want news
-      // Insert querry
-    if($Send != '0'){$Send = time();}
-    $db_conn->query("UPDATE NewsLetter SET Subject = '$Title', Body = '$Body', SentDate = '$Send', Author = '$Aurthor'
-                     WHERE LetterID = '$URLID'");
-    }// if $Send = 1
-    else{
-      $db_conn->query("UPDATE NewsLetter SET Subject = '$Title', Body = '$Body', SentDate = '0', Author = '$Aurthor'
-                     WHERE LetterID = '$URLID'");
-    }
-
-    // update querry
-    //$db_conn->query("UPDATE NewsLetter SET Subject = '$Title', Body = '$Body', SentDate = '$Send', Author = '$Aurthor'
-      ///               WHERE LetterID = '$URLID'");
-    header("Location: index.php?page=Admin&subpage=NewsLetter#admin_menu");
+  if(isset($_POST['Members'])){$membersOnly = $db_conn->real_escape_string($_POST['Members']);}else {$membersOnly = '0';}
+  #$Body  = $db_conn->real_escape_string($_POST['AdminTinyMCE']); # = what to put in the body of the mail function
+  $Body  = $_POST['AdminTinyMCE']; # = what to put in the body of the mail function
+  $Aurthor = $_SESSION['UserID']; # is saved to see whom there have made this newsletter
+  if ($Send == 1) {$Send = time();} ## If "udsend" Checkbox is checked then defined $Send as Unixtimestamp
+  # ==== Edit or New/copy ====
+  if ($action == "Edit") {
+    # Update statement
+    $NewsLetterStatement = $db_conn->prepare("UPDATE NewsLetter SET `Subject` = ?, `Body` = ?, `SentDate` = '$Send', `Author` = '$Aurthor', MembersOnly = '$membersOnly'  WHERE `LetterID` = ?");
+    $NewsLetterStatement->bind_param("ssi", $Title, $Body, $URLID);    
+  }else {
+    $NewsLetterStatement = $db_conn->prepare("INSERT INTO `NewsLetter`(`Subject`, `Body`, `SentDate`, `Author`, `MembersOnly`) VALUES (?,?,'$Send','$Aurthor','$membersOnly')");
+    $NewsLetterStatement->bind_param("ss", $Title, $Body);
+  }
+  # ==========================
+  $Recivers = array();
+  if ($membersOnly == 1) {
+    # if Member is checked
+    $y = date("Y");
+    $ReciversSTMT = $db_conn->prepare("SELECT Users.Email From UserMembership Inner Join Users On UserMembership.UserID = Users.UserID Where UserMembership.Year = '$y'");
   }else{
-    if($Send == 1){
-
-      $NewsResult = $db_conn->query("SELECT Users.FullName, Users.Email, Users.NewsLetter FROM Users WHERE Users.NewsLetter = 1");
-        while($NewsRow = $NewsResult->fetch_assoc()){
-          // Send mail
-          SendMail($NewsRow["Email"],$NewsRow["FullName"],$Title,$Body,$_GLOBAL);
-          //echo "<hr>";
-        }// End of Users that want news
-      
-      // Insert querry
-    if($Send != '0'){$Send = time($Send);}
-    $db_conn->query("INSERT INTO NewsLetter (Subject, Body, SentDate, Author)
-                                     VALUES ('$Title','$Body','$Send','$Aurthor')");
-    }// if $Send = 1
-    else{
-      $db_conn->query("INSERT INTO NewsLetter (Subject, Body, SentDate, Author)
-                                     VALUES ('$Title','$Body','0','$Aurthor')");
+    $ReciversSTMT = $db_conn->prepare("SELECT Users.Email From Users Where NewsLetter = 1");
+  }
+  $ReciversSTMT->bind_result($email);
+  $ReciversSTMT->execute();
+  while($ReciverEmail = $ReciversSTMT->fetch()) {
+    $Recivers[] .= $email;
+  }
+  # ==========================
+  if($NewsLetterStatement->execute()){
+    # IF sucessfull inserted into Database continue with doing more stuff.
+    if ($Send != 0) {
+      # if "Udsend" is checked
+      $Key        = $_GLOBAL['MailgunKey'];
+      $HTML       = $Body;#Insert body from Newsletter
+      #$To         = "torsoya@gmail.com";#define this when calling the functions instead of here for newsletters
+      $From       = $_GLOBAL['SendMailFrom'];
+      $Subject    = $Title;#Insert Subject from Newsletter
+      $Sending = 1;
+      foreach ($Recivers as $key => $value) {
+        MailGunSender($From, $value, $Subject, $HTML, $Key);
+        echo "$value er sat i kø.";
+        echo "</br>";
+        echo '<meta http-equiv="refresh" content="10; url=?page=Admin&subpage=NewsLetter#admin_menu" />';
+      }
     }
-  }// if action is not Edit
-  header("Location: index.php?page=Admin&subpage=NewsLetter#admin_menu");
+  }else {
+    echo "NewsLetter did not get inserted into the DB and nothing else was done";
+  }
 }// if submmit send
 
+#=====================================================================
+## Interface starts here for creating/deiting of Newsletter
+#=====================================================================
 // edit or template
 if(isset($_GET['action']) && ( ($_GET['action'] == 'Edit') || ($_GET['action'] == 'Template') ) ){
   $URLID = $db_conn->real_escape_string($_GET['id']);
@@ -61,7 +68,7 @@ if(isset($_GET['action']) && ( ($_GET['action'] == 'Edit') || ($_GET['action'] =
   if( ($action == 'Edit') && ($row['SentDate'] > 0) ){
     $LetterExist = 0;
     //echo '<b><p class="text-center"> Det valgte nyheds brev kan ikke redigers, da det er blevet udsendt</p></b>';
-    header("Location: index.php?page=Admin&subpage=NewsLetter#admin_menu"); // back to newsletter list
+   # header("Location: index.php?page=Admin&subpage=NewsLetter#admin_menu"); // back to newsletter list
   }else{
     $LetterExist = 1;
     $Subject  = $row['Subject'];
@@ -69,27 +76,34 @@ if(isset($_GET['action']) && ( ($_GET['action'] == 'Edit') || ($_GET['action'] =
     $sent     = $row['SentDate'];
   }
 }
+if ($Sending == 0) {
 
-if( (isset($LetterExist) && $LetterExist == 1) || $action == 'New')
-{
-?>
-<form method="post" action="">
-  <div class="form-group col-lg-6">
-    <label class="control-label" for="Title">Emne: </label>
-    <input class="form-control" type="text" maxlength="50" name="Title" id="Title" required size="50" value="<?php if($action == 'Edit'){echo $Subject;} ?>">
-  </div>
-  <div class="form-group form-inline col-lg-6">
-    <label>Udsend:</label> 
-    <input type="checkbox" value="1" name="Send">
-  </div>
-  <div class="form-group col-lg-12">
-    <textarea rows="25" id="AdminTinyMCE" name="AdminTinyMCE"><?php if(isset($LetterExist)){echo $Body;} ?></textarea>
-  </div>
-  <div class="form-group text-center col-lg-12">
-    <input class="btn btn-default" type="submit" value="Gem" name="Save" />
-  </div>
-</form>
-<?php
+  if( (isset($LetterExist) && $LetterExist == 1) || $action == 'New')
+  {
+  ?>
+  <form method="post" action="">
+    <div class="form-group col-lg-6">
+      <label class="control-label" for="Title">Emne: </label>
+      <input class="form-control" type="text" maxlength="50" name="Title" id="Title" required size="50" value="<?php if($action == 'Edit'){echo $Subject;} ?>">
+    </div>
+    <div class="form-group form-inline col-lg-3">
+      <label>Udsend:</label> 
+      <input type="checkbox" value="1" name="Send">
+    </div>
+    <div class="form-group form-inline col-lg-3">
+      <label>Medlemmer:</label> 
+      <input type="checkbox" value="1" name="Members">
+    </div>
+    <div class="form-group col-lg-12">
+      <textarea rows="25" id="AdminTinyMCE" name="AdminTinyMCE"><?php if(isset($LetterExist)){echo $Body;} ?></textarea>
+    </div>
+    <div class="form-group text-center col-lg-12">
+      <input class="btn btn-default" type="submit" value="Gem" name="Save" />
+    </div>
+  </form>
+  <?php
+  }
+  
 }
 ?>
 
