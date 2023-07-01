1455eb66d2a246b298dc982536b398fdfb6c7bc2
Tor Soya <torsoya@gmail.com>
2023-07-01 10:51:34 +0200

    Update Convert.php

--- Include/TestArea/Convert.php
+++ Include/TestArea/Convert.php
@@ -1,23 +1,35 @@
 <?php
-$db_host="157.90.34.121"; // Host name
+$db_host="localhost"; // Host name
 //$db_host="localhost"; // Host name
-$db_username="topperto_LanCMS"; // Mysql username
-$db_password="o619Jbv4xGq7hP3I3q^322Afp"; // Mysql password
-$db_name="topperto_LanCMS"; // Database name
+$db_username="root"; // Mysql username
+$db_password=""; // Mysql password
+$db_name="lancms_convertdb"; // Database name
 $db_conn = new mysqli("$db_host","$db_username","$db_password","$db_name");
 $db_conn -> set_charset("utf8");
 
 
 
 
-$result = $db_conn->query("SELECT * FROM News ");
+$result = $db_conn->query("SELECT * FROM newsletters ");
 
 while ($row = $result->fetch_assoc()) {
-    echo $id     = $row["NewsID"].'</br>';
-    $created      = $row["CreatedDate"];
-    #$laslogin   = strtotime($row["lastlogin"]);
-    #$AuthorID = $row["AuthorID"];
+    $id     = $row["id"];
+    $created      = strtotime($row["created_date"]);
+    
+    
+    
+    $db_conn->query("Update newsletters SET created_date = '$created' Where id = '$id'");
 
+    
+    #$AuthorID = $row["AuthorID"];       
     #$db_conn->query("Update News SET LastEditedID = '$AuthorID' Where NewsID = '$id'");    
-    $db_conn->query("Update News SET PublishDate = '$created' Where NewsID = '$id'");    
+    
+    
+    # news table convert from hlparty.dk 2008 ->
+
+    # User table convert from hlparty.dk 2008 -> LanCMS
+    #$id         = $row["id"];
+    #$laslogin   = strtotime($row["lastlogin"]);
+    #$added      = strtotime($row["added"]);
+    #$db_conn->query("Update users SET added = '$added', lastlogin = '$laslogin' WHERE id = '$id'"); 
 }
