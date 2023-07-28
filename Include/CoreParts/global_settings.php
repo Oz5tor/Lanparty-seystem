3f8871104de55e9a1c2cd777de9ff9ec0b21ac0b
Tor Soya <torsoya@gmail.com>
2023-07-28 11:45:26 +0200

    define event information GLOBAL
    now always taking the higest EventID from Database. not optimal but im lazy

--- Include/CoreParts/global_settings.php
+++ Include/CoreParts/global_settings.php
@@ -15,5 +15,11 @@ $row2 = $result2->fetch_assoc();
 if(!$result2->num_rows == 0){
     $_GLOBAL['EventID'] = $row2['EventID'];
     $_GLOBAL['EventName'] = $row2['Title'];
+}else{
+  $query2 = "Select * from Event ORDER BY EventID DESC LIMIT 1";
+  $result2 = $db_conn->query($query2);
+  $row2 = $result2->fetch_assoc();
+  $_GLOBAL['EventID'] = $row2['EventID'];
+    $_GLOBAL['EventName'] = $row2['Title'];
 }
 #$_GLOBAL['OneAllActive'] = 1;
