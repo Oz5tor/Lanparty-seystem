ad60901054af12d410f6981c3940f7028fa9c8db
Tor Soya <torsoya@gmail.com>
2023-07-28 11:38:53 +0200

    UserBox Update
    Change the DOM setup of the userbox better viewport optimization
    
    nb!: still not happy with the profile image, quite small

--- Include/Usermodule/Userbox.php
+++ Include/Usermodule/Userbox.php
@@ -9,30 +9,49 @@
         if($row['ProfileIMG'] == ''){ $PictureUrl = "Images/Users/nopic.png";} else { $PictureUrl = $row['ProfileIMG']; }
       }
     }
+
+
+    ## Get seat number
+    $Userbox_HasTicket = 0;
+
+    if(isset($_GLOBAL['EventID'])){
+      $checkSeat = $db_conn->query("SELECT SeatNumber FROM tickets WHERE RevokeDate IS NULL AND EventID = '".$_GLOBAL['EventID']."' AND UserID = '".$_SESSION['UserID']."' ");
+      if($checkSeat -> num_rows){
+        $Myseat = $checkSeat->fetch_assoc();
+        #echo $Myseat["SeatNumber"];
+      }
+      
+    }
+
+
     ?>
 
-<div class="row LanCMScontentbox align-items-center">
-  <div class="col-lg-2 col-md-2 col-sm-2 col-xs-7 ">
-  <img height="75" src="<?php echo $PictureUrl; ?>">
-    <?php
-#    echo "Hej ".$PreffereredUsername."<br>";
-#    echo $FullName;
-#    echo $Clan;
-    ?>
+<div class="row LanCMScontentbox">
+  <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+  <img class="img-responsive" src="<?php echo $PictureUrl; ?>">
+
   </div>
-  <div class="col-lg-10 col-md-10 col-sm-10 col-xs-7  ">
-      <!-- <a class="btn btn-info" style="margin-bottom: 10px;" href="">Min billet</a> -->
-      <a class="btn btn-primary" style="margin-bottom: 10px;" href="?page=EditMyProfile">Ret min profil</a>
-  <?php if(isset($_SESSION['Admin']) && ($_SESSION['Admin'] == 1)){ ?> 
-      <a class="btn btn-warning" style="margin-bottom: 10px;" href="?page=Admin">Admin</a>
-      <?php } ?>
-      <a class="btn btn-danger" style="margin-bottom: 10px;" href="?action=LogOut">Logud</a>
-      <p>
+  <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10">
+    <div class="row">
+      <a class="btn btn-primary" role="button" href="?page=EditMyProfile">Ret min profil</a>
+    <?php if(isset($_SESSION['Admin']) && ($_SESSION['Admin'] == 1)){ ?> 
+      <a class="btn btn-warning" role="button" href="?page=Admin">Admin</a>
+    <?php } ?>
+      <a class="btn btn-danger" role="button" href="?action=LogOut">Logud</a>
+    </div>
+    <div class="row">
       <?php 
-          echo "Hej ".$PreffereredUsername.".";
-      ?>
-      </p> 
+            echo "Hej ".$PreffereredUsername.". ";
+            if(isset($_GLOBAL["EventID"])){
+              if(isset($Myseat["SeatNumber"])){
+                echo "| Din plads er: ".$Myseat["SeatNumber"];
+              }
+            }
+        ?>
+    </div> 
   </div>  
 </div>
+
+
 
 
--- index.php
+++ index.php
@@ -22,32 +22,21 @@ require_once("Include/CoreParts/global_settings.php");
 </head>
 <body>
     <?php include_once("Include/TestArea/DEBUGGIN.php"); ?>
-
-    <!-- Facebook scocial like code prep start
-    <div id="fb-root"></div>
-    <script>(function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/da_DK/sdk.js#xfbml=1&version=v2.7&appId=1480239178911395";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
-    Facebook scocial like code prep end -->
     <!-- Slider start
     <div class="LanCMSno_margin_padding hidden-xs container-fluid">
        <img src="Images/image-slider-5.jpg" class="img-responsive center-block" >
     </div>
     Slider end -->
-    <header id="header">
+    <div class="container-fluid" id="header">
         <!-- Top start -->
         <?php require_once("Include/TopHeader.php"); ?>
         <!-- Top end -->
-        <br>
+    </div>
+    <header id="">
         <!-- Nav start -->
         <?php require_once("Include/NavigationBar.php"); ?>
         <!-- Nav end -->
     </header>
-    <br>
     <?php
     if (isset($_SESSION['MsgForUser'])) {
         include_once 'Include/MsgUser.php';
